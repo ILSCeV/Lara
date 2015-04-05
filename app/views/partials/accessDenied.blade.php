175bf713f83257e9f1b6bac58d5e9d1bd07f0d4b
MD <4d44hex@gmail.com>
2015-04-05 20:44:32 +0200

    Cleanup

--- app/views/partials/accessDenied.blade.php
+++
@@ -1,2 +0,0 @@
-<div class="panel">{{ Config::get('messages_de.access-denied') }}
-</div>
--- app/views/partials/filter.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="pull-right" id="filter">
-        <div class="checkbox">
-          <label><input type="checkbox" id="bc-Club" checked>bc-Club anzeigen</label>
-        </div>
-        <div class="checkbox">
-          <label><input type="checkbox" id="bc-Café" checked>bc-Café anzeigen</label>
-        </div>
-</div>
