15f1b635a03ce6e59d1c6f6e3e8d063c0c937ac8
MD <4d44hex@gmail.com>
2015-11-05 20:54:48 +0100

    Add Lara assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/favicon-48x48.png') }}">
+
         <style>
             html, body {
                 height: 100%;
