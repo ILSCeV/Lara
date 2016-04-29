5908083ea92b838a74b969714a8b497eb1f530e3
Markus Postler <markus.postler@tu-ilmenau.de>
2016-04-29 14:34:33 +0200

    add surveyView and editSurveyView, add example forms to createSurveyView

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -12,23 +12,8 @@ class SurveyController extends Controller
     {
         //;
     }
-    public function create($year = null, $month = null, $day = null, $templateId = null)
+    public function create()
     {
-        // Filling missing date and template number in case none are provided
-        if ( is_null($year) ) {
-            $year = date("Y");
-        }
-
-        if ( is_null($month) ) {
-            $month = date("m");
-        }
-
-        if ( is_null($day) ) {
-            $day = date("d");
-        }
-
-        // prepare correct date format to be used in the forms
-        $date = strftime("%d-%m-%Y", strtotime($year.$month.$day));
         return view('createSurveyView');
     }
 
