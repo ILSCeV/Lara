8d9cff6795d4d4943023d50676cb1301c644edf9
Markus Postler <markus.postler@tu-ilmenau.de>
2016-04-30 22:54:41 +0200

    added fillables and getPerson functions to Models

--- app/Survey.php
+++ app/Survey.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     protected $table ='survey';
-
-    //one Survey can have many SurveyQuestions
-    public function questions()
+    protected $fillabe = array('deadline');
+    
+    public function getQuestions()
     {
         return $this->hasMany('App\SurveyQuestion');
     }
-    
+
+    public function getPerson()
+    {
+        return $this->belongsTo('App\Person', 'prsn_id', 'id');
+    }
 }
--- app/SurveyAnswer.php
+++ app/SurveyAnswer.php
@@ -7,17 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyAnswer extends Model
 {
     protected $table ='survey_answer';
-
-    //many SurveyAnswers in one SurveyQuestion possible
-    public function questionNumber()
+    protected $fillable = array('name', 'content');
+    
+    public function getQuestion() 
     {
         return $this->belongsTo('App\SurveyQuestion', 'SurveyQuestion_number', 'number');
     }
 
-    //return Survey of the answer, don't know if this works properly at the moment
-    public function survey()
+    //returns Survey of the answer, don't know if this works properly at the moment
+    public function getSurvey() 
     {
         return $this->belongsTo('App\SurveyQuestion', 'SurveyQuestion_Survey_id', 'Survey_id')->belongsTo('App\Survey');
+    }
+    
+    public function getPerson()
+    {
+        return $this->belongsTo('App\Person', 'prsn_id', 'id');
     }
 
 }
