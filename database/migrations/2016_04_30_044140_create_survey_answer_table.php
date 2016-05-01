9c1c04191acf032ae9627592a891b577ab6dda1d
Ludwig Breitsprecher <jaufe95-git@yahoo.de>
2016-05-01 15:38:17 +0200

    fixed foreign key names in "survey_answer"

--- database/migrations/2016_04_30_044140_create_survey_answer_table.php
+++ database/migrations/2016_04_30_044140_create_survey_answer_table.php
@@ -13,8 +13,8 @@ class CreateSurveyAnswerTable extends Migration
     public function up()
     {
         Schema::create('survey_answer', function (Blueprint $table) {
-            $table->integer('survey_id')->references('survey_id')->on('survey_question')->unsigned();
-            $table->integer('survey_number')->references('number')->on('survey_question')->unsigned();
+            $table->integer('survey_question_id')->references('survey_id')->on('survey_question')->unsigned();
+            $table->integer('survey_question_number')->references('number')->on('survey_question')->unsigned();
             $table->primary(['survey_id', 'survey_number']);
             $table->integer('prsn_id')->references('persons')->on('id')->unsigned();
             $table->string('name');
