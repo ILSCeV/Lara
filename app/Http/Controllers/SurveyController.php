212c1604b20bbd1f5cacb66c38e1effc3c5ad8b0
Markus Postler <markus.postler@tu-ilmenau.de>
2016-05-03 18:48:26 +0200

    added support for surveyQuestions, some fixes

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,7 +2,7 @@
 
 namespace Lara\Http\Controllers;
 
-use Carbon\Carbon;
+use Lara\SurveyQuestion;
 use Request;
 
 use Lara\Http\Requests;
@@ -17,13 +17,13 @@ class SurveyController extends Controller
     public function index()
     {
         $surveys = Survey::all();
-        
-        return $surveys;
+        foreach($surveys as $survey)
+            $questions = $survey->getQuestions;
+        return $questions;
     }
     public function create($year = null, $month = null, $day = null)
     {
-
-        //dont creates surveys, just return the createSurveyView with todays date
+        //dont creates surveys, just returns the createSurveyView with todays date
         // Filling missing date in case none is provided
         if ( is_null($year) ) {
             $year = date("Y");
@@ -39,8 +39,6 @@ class SurveyController extends Controller
 
         // prepare correct date format to be used in the forms
         $date = strftime("%d-%m-%Y", strtotime($year.$month.$day));
-
-
         return View('createSurveyView', compact('date'));
     }
 
@@ -51,38 +49,80 @@ class SurveyController extends Controller
 
     public function show($id)
     {
+        //find survey
         $survey = Survey::findOrFail($id);
 
-        //questions and answers need to be found and returned to View
+        //find questions
+        $questions = $survey->getQuestions;
 
-        return view('surveyView', compact('survey'));
+        //maybe finds only answers for a single question? need to check this
+        foreach($questions as $question)
+            $answers = $question->getAnswers;
+
+        return view('surveyView', compact('survey', 'questions', 'answers'));
     }
 
-    public function update()
+    public function update($id)
     {
-        //;
+        //find survey
+        $survey = Survey::findOrFail($id);
+
+        //find questions and answers
+        $questions = Survey::findOrFail($survey->getQuestions->id);
+        $answers = SurveyQuestion::findOrFail($questions->getAnswers->survey_question_number);
+
+        //edit survey
+        $survey = $this->editSurvey($id);
+
+        //save everything
+        $survey->save();
+
+        /*
+        foreach($questions as $question)
+        {
+            $question->save;
+            foreach ($answers as $answer)
+                $answer->save();
+        }
+        */
+        return view('surveyView', compact('survey','questions','answers'));
     }
 
-    public function edit()
+    public function edit($id)
     {
-        //;
+        //find survey
+        $survey = Survey::findOrFail($id);
+
+        //find questions and answers
+        $questions = Survey::findOrFail($survey->getQuestions->id);
+        $answers = SurveyQuestion::findOrFail($questions->getAnswers->survey_question_number);
+
+        return view('editSurveyView', compact('survey','questions','answers'));
     }
     public function store()
     {
-        //stores default survey, needs user input in future
+        //load default
         $newSurvey = $this->editSurvey(null);
 
-        Survey::create([$newSurvey]);
+        $i = Input::get('number_of_questions');
+
+        //create and save the questions with the actual survey id
+        //$i = 2; //for testing purpose
+        for($i; $i>0; $i--)
+            new SurveyQuestion([$newSurvey->id]);
+
+        $newSurvey->save();
+
+        //calls index(), later we better show the surveyView($newSurvey->id)
         return redirect('survey');
     }
-
 
     //private function, can only be called within the Controller
     private function editSurvey($id)
     {
         $survey = new Survey;
 
-        //get existing survey by id
+        //get existing survey by id if provided
         if (!is_null($id))
         {
             $survey = Survey::findOrFail($id);
@@ -94,14 +134,15 @@ class SurveyController extends Controller
         //update deadline
         if (!empty(Input::get('deadline')))
         {
-            $newDeadline = new DateTime(Input::get('deadline'), new DateTimeZone(Config::get('app.timezone')));
+            $newDeadline = new DateTime(Input::get('deadline'), mktime(0, 0, 0, 12, 31, 2100));
             $survey->deadline = $newDeadline->format('Y-m-d');
         }
+
         //if deadline is empty set new deadline, doesnt work at the moment
         else
         {
             $survey->deadline = date('Y-m-d', mktime(0, 0, 0, 12, 31, 2100));
         }
-        return $survey;
+        return view('surveyView', compact($survey->id));
     }
 }
