5edc5712f98bea962bcda39b99d636445eb7c32a
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-05-06 21:16:33 +0200

    add basic example surveyVIew

--- resources/views/surveyView.blade.php
+++ resources/views/surveyView.blade.php
@@ -6,4 +6,24 @@
 
 @section('content')
     <h1>Hier soll eine einzelne Umfrage angezeigt werden</h1>
+    <div>
+        {{ $survey->title }}
+    </div>
+    <div>
+        {{ $survey->description }}
+    </div>
+    <div>
+        Die Umfrage läuft bis:
+        {{ $survey->deadline }}
+    </div>
+
+    <div>
+        Fragen:
+    </div>
+    @foreach($questions as $question)
+        <div>
+            {{$question->content}}
+        </div>
+    @endforeach
+
 @stop
