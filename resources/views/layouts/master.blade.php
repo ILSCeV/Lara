472abe89cf5a03ca2446816fcfab8869015269be
CAJan93 <jan.mensch@tu-ilmenau.de>
2016-05-10 22:08:08 +0200

    montView verändert bei master.blade den Verweis auf Custom.css hinzugefügt

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" media="all" type="text/css" href="{{ asset('/css/font-awesome.min.css') }}" />
         <link rel="stylesheet" media="all" type="text/css" href="{{ asset('/css/vedst.css') }}" />
         <link rel="stylesheet" media="print" type="text/css" href="{{ asset('/css/print.css') }}" />
+        <link rel="stylesheet" media="print" type="text/css" href="{{ asset('/css/custom.css') }}" />
 
     	<link rel="shortcut icon" type="image/png" href="{{ asset('/favicon-48x48.png') }}">
 
