f4114c62c76f2bdf5c9b298d4fa6cadaa6f56067
Achim Bode <achim.bode@tu-ilmenau.de>
2016-05-13 15:36:35 +0200

    Umfragen werden jetzt nach der Erstellung im Kalender angezeigt.
    Design wird natürlich später noch angepasst.

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -61,6 +61,7 @@ class SurveyController extends Controller
             $survey->description);
 
         $survey->deadline = strftime("%Y-%m-%d %H-%M-%S", strtotime($input->deadline));
+        $survey->begin = strftime("%Y-%m-%d", strtotime($input->begin));
         $survey->save();
 
         foreach($input->questions as $number => $question){
@@ -73,6 +74,7 @@ class SurveyController extends Controller
         }
 
         return Redirect::action('SurveyController@create');
+
     }
 
 
