b83521a8d0c121f0473d056a64c382e1c6f63863
Jan Mensch <jan.mensch@tu-ilmenau.de>
2016-05-17 23:58:10 +0200

    weiteres

--- resources/views/monthView.blade.php
+++ resources/views/monthView.blade.php
@@ -90,32 +90,34 @@
             <!-- Show Days  -->
     @if($i - $date['startDay'] >= 0 AND $i-$date['startDay'] < $date['daysOfMonth'])
             <!-- color current week gray -->
-    @if ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
-        <div class="thisMonth today-marker custom-md-85">
-            @else
-                <div class="thisMonth custom-md-85">
-                    @endif
-                    <div class="cell10">
-                        {!! $simpleDate !!}
-                        <?php $simpleDate++; ?>
-                    </div>
-                    <div class="cell90">
-                        @include ('partials.calendarCellDate', $date)
-                    </div>
+    @if(date("Y-m-d", strtotime($i - $date['startDay']." day", $date['startStamp'])) === date( "Y-m-d" ) )
+        <div class="thisMonth today-marker-today custom-md-85">
+            @elseif ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
+                <div class="thisMonth today-marker custom-md-85">
+                    @else
+                        <div class="thisMonth custom-md-85">
+                            @endif
+                            <div class="cell10">
+                                {!! $simpleDate !!}
+                                <?php $simpleDate++; ?>
+                            </div>
+                            <div class="cell90">
+                                @include ('partials.calendarCellDate', $date)
+                            </div>
+                        </div>
+                        @else
+                            <div class="otherMonth custom-md-85 empty">
+                                <!-- date("j", strtotime($i-$date['startDay']." day", $date['startStamp'])) -->
+                            </div>
+                            @endif
+
+                                    <!-- if sunday then end this line -->
+                            @if(date("N",date("j", strtotime($i-$date['startDay']." day", $date['startStamp']))) == 7)
+
+                            @endif
+
+                            @endfor
+
                 </div>
-                @else
-                    <div class="otherMonth custom-md-85 empty">
-                        <!-- date("j", strtotime($i-$date['startDay']." day", $date['startStamp'])) -->
-                    </div>
-                    @endif
-
-                            <!-- if sunday then end this line -->
-                    @if(date("N",date("j", strtotime($i-$date['startDay']." day", $date['startStamp']))) == 7)
-
-                    @endif
-
-                    @endfor
-
         </div>
-</div>
 @stop
