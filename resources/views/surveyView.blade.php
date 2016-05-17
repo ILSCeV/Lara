4ce88d8441a5126ef67932ff225f2607e415c026
Achim Bode <achim.bode@tu-ilmenau.de>
2016-05-17 12:01:20 +0200

    Neues Symbol in der Wochenansicht für Umfragen
    & kleinere Änderungen zur Auswertung bzw. Anzeige der Antworten von Umfragen

--- resources/views/surveyView.blade.php
+++ resources/views/surveyView.blade.php
@@ -35,41 +35,12 @@
             @endforeach
         </div>
     </div>
-
-        <div class="row">
-            <div class="panel padding-left-minimal">
-            <h5 class="panel-title text-center">Antwort</h5>
-                <form method="POST" action="/survey/{{ $survey->id }}/storeAnswer">
-
-                    <div class="form-group">
-                        <h6>Hier Antwort eingeben:</h6>
-                        <textarea name="answer" class="form-control"></textarea>
-                    </div>
-                    <input name="survey_id" type="hidden" value="{{ $survey->id }}" />
-                    <div class="form-group">
-                    </div>
-            </div>
-                        <button type="submit" class="btn btn-primary">Abstimmen</button>
-            <a href="javascript:history.back()" class="btn btn-default">Ohne Änderung zurück</a>
-                    </div>
-                    <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-                </form>
-            </div>
-
-
-
-
-<div class="btn-group col-md-6">
-        <br class="visible-xs visible-sm">
-        <div class="panel">
-            <div class="panel-heading">
-                <h4 class="panel-title">Bisherige Abstimmungen:</h4>
-            </div>
-            <div class="panel-body">
-            <div class="form-group">
-                    <i class="text-danger">Auswertung</i>
-                    </div>
-                </div>
-            </div>
+        <div>
+            @include('partials.surveyAnswer')
         </div>
+
+                <div>
+                    @include('partials.surveyEvaluation')
+                </div>
+
 @stop
