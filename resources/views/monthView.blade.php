5c8dad18e4a7b092c66e46f7ec653672d0d2ee49
CAJan93 <jan.mensch@tu-ilmenau.de>
2016-05-20 21:10:42 +0200

    Revert "monthView.blade added missing </div> s"
    This reverts commit 1ecd029cd72abb8b1bb85d4c8ae4918e840ebb02.

--- resources/views/monthView.blade.php
+++ resources/views/monthView.blade.php
@@ -4,7 +4,6 @@
 @section('title')
 {{ $date['monthName'] . " " . $date['year'] }}
 @stop
-
 @section('content')
 
         <!-- prev/next month -->
@@ -43,7 +42,7 @@
     <div class="col-md-1">
     </div>
     <div class="col-md-11 calendarWrapper">
-        <div class=" hidden-xs" id="ContentRow">
+        <div class=" hidden-xs" id="ContentRow" >
             <div class=" kalenderWoche">
                 KW
             </div>
@@ -76,63 +75,60 @@
                 <!--define row-->
         @if($i == 1)
             <div class="calendarRow clearfix group height10vh">
-                @elseif( $i == 8 || $i == 15 || $i == 22 || $i == 29 || $i == 36)
-                    <div class="calendarRow clearfix group">
-                        @endif
-                                <!--End define row at bottom-->
+        @elseif( $i == 8 || $i == 15 || $i == 22 || $i == 29 || $i == 36)
+               <div class="calendarRow clearfix group">
+        @endif
+                <!--End define row at bottom-->
 
-                        <!-- Weeks on left side -->
-                        @if(date("N", strtotime($i - $date['startDay'] . " day", $date['startStamp'])) == 1)
-                            @if ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
-                                <div class="custom-md-85 Tag WeekMarker">
-                                    @else
-                                        <div class=" custom-md-85 Tag">
-                                            @endif
-                                            <a href="{!! Request::getBasePath() !!}/calendar/{!! $date['year'] !!}/KW{{ date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) }}">
-                                                KW {{ date("W", strtotime($i - $date['startDay'] . " day", $date['startStamp'])) }}
-                                            </a>
-                                        </div>
-                                        @endif
-
-
-                                                <!-- Show Days  -->
-                                        @if($i - $date['startDay'] >= 0 AND $i-$date['startDay'] < $date['daysOfMonth'])
-
-                                                <!-- color current week gray -->
-                                        @if(date("Y-m-d", strtotime($i - $date['startDay']." day", $date['startStamp'])) === date( "Y-m-d" ) )
-                                            <div class="thisMonth today-marker-today custom-md-85">
-                                                @elseif ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
-                                                    <div class="thisMonth today-marker custom-md-85">
-                                                        @else
-                                                            <div class="thisMonth custom-md-85">
-                                                                @endif
-                                                                <div class="cell10">
-                                                                    {!! $simpleDate !!}
-                                                                    <?php $simpleDate++; ?>
-                                                                </div>
-                                                                <div class="cell90">
-                                                                    @include( 'partials.calendarCellDate', $date)
-                                                                </div>
-                                                            </div>
-                                                            @else
-                                                                <div class="otherMonth custom-md-85 empty">
-                                                                    <!-- date("j", strtotime($i-$date['startDay']." day", $date['startStamp'])) -->
-                                                                </div>
-                                                                @endif
-
-                                                                        <!-- if sunday then end this line -->
-                                                                @if(date("N",date("j", strtotime($i-$date['startDay']." day", $date['startStamp']))) == 7)
-
-                                                                @endif
-
-                                                                @if($i == 7 || $i == 14 || $i == 21 || $i == 28 || $i == 35 || $i == 42)
-                                                    </div>
-                                                @endif
-                                                @endfor
-                                            </div>
-                                </div>
-                    </div>
-            </div>
+        <!-- Weeks on left side -->
+        @if(date("N", strtotime($i - $date['startDay'] . " day", $date['startStamp'])) == 1)
+            @if ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
+                <div class="custom-md-85 Tag WeekMarker" >
+                @else
+                        <div class=" custom-md-85 Tag">
+            @endif
+            <a href="{!! Request::getBasePath() !!}/calendar/{!! $date['year'] !!}/KW{{ date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) }}">
+                KW {{ date("W", strtotime($i - $date['startDay'] . " day", $date['startStamp'])) }}
+            </a>
     </div>
-</div>
+    @endif
+
+
+            <!-- Show Days  -->
+    @if($i - $date['startDay'] >= 0 AND $i-$date['startDay'] < $date['daysOfMonth'])
+            <!-- color current week gray -->
+    @if(date("Y-m-d", strtotime($i - $date['startDay']." day", $date['startStamp'])) === date( "Y-m-d" ) )
+        <div class="thisMonth today-marker-today custom-md-85">
+            @elseif ( date('W', strtotime($i - $date['startDay'] . ' day', $date['startStamp'])) === date("W") )
+                <div class="thisMonth today-marker custom-md-85">
+                    @else
+                        <div class="thisMonth custom-md-85">
+                            @endif
+                            <div class="cell10">
+                                {!! $simpleDate !!}
+                                <?php $simpleDate++; ?>
+                            </div>
+                            <div class="cell90">
+                                @include( 'partials.calendarCellDate', $date)
+                            </div>
+                        </div>
+                        @else
+                            <div class="otherMonth custom-md-85 empty">
+                                <!-- date("j", strtotime($i-$date['startDay']." day", $date['startStamp'])) -->
+                            </div>
+                            @endif
+
+                                    <!-- if sunday then end this line -->
+                            @if(date("N",date("j", strtotime($i-$date['startDay']." day", $date['startStamp']))) == 7)
+
+                            @endif
+
+                            @if($i == 7 || $i == 14 || $i == 21 || $i == 28 || $i == 35 || $i == 42)
+                                </div>
+                            @endif
+
+                            @endfor
+
+                </div>
+        </div>
 @stop
