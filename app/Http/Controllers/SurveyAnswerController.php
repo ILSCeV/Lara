320cc29006862d46c01baf5eebfda44abc5dcc47
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-05-21 12:32:40 +0200

    Fix SurveyQuestion->getAnswers, add test functions to SurveyAnswerController

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -5,6 +5,8 @@ namespace Lara\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Lara\Http\Requests;
+use Lara\Survey;
+use Lara\SurveyAnswer;
 
 class SurveyAnswerController extends Controller
 {
@@ -13,14 +15,23 @@ class SurveyAnswerController extends Controller
      */
     public function show($surveyid, $id)
     {
+        $input = ['name' => 'Hans',
+                  'answer' => [0 => 'testantwort 1', 1 => 'testantwort 2']
+                 ];
         var_dump($surveyid, $id);
+        $survey = Survey::findOrFail($surveyid);
+        foreach($input['answer'] as $answer) {
+            var_dump($answer);
+            $survey_answer = new SurveyAnswer();
+        }
     }
     /**
+     * @param int $surveyid
      * @param Request $input
      */
-    public function store(Request $input)
+    public function store($surveyid, Request $input)
     {
-
+        var_dump($input->all());
     }
 
     public function update($id)
