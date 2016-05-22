124e6dc59a030eda8e15bea3b0091edd1508eb92
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-05-22 20:41:11 +0200

    Fix double dates in monthView

--- resources/views/partials/monthCell.blade.php
+++ resources/views/partials/monthCell.blade.php
@@ -1,19 +1,5 @@
 {{-- Needs variables: $surveys, $events --}}
 
-@if(Session::has('userGroup'))
-    <a href="{{ Request::getBasePath() }}/
-			event/
-			{{ $date['year']}}/
-			{{ $date['month'] }}/
-			{{ strftime("%d", strtotime($i - $date['startDay']." day", $date['startStamp'])) }}/
-			0/
-			create">
-	{{ date("j.", strtotime($i - $date['startDay']." day", $date['startStamp'])) }}
-	</a>
-@else
-	{{ date("j.", strtotime($i - $date['startDay']." day", $date['startStamp'])) }}
-@endif
-
 @foreach($surveys as $survey)
 	@if($survey->in_calendar === date("Y-m-d", strtotime($i - $date['startDay']." day", $date['startStamp'])))
 		<div class="cal-event dark-grey calendar-internal-info">
