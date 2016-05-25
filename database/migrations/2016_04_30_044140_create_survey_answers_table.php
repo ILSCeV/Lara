c37007478a0f849d8baeb1d6ac6d0e6719b7ec83
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-05-25 15:46:27 +0200

    adjust database migrations to fit ERM
    - table names are plural
    - column names are all snake_case
    - some column names are renamed to make understanding the purpose easier
    - add boolean options in surveys table

--- database/migrations/2016_04_30_044140_create_survey_answers_table.php
+++ database/migrations/2016_04_30_044140_create_survey_answers_table.php
@@ -1,9 +1,10 @@
+
 <?php
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSurveyTable extends Migration
+class CreateSurveyAnswerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +13,14 @@ class CreateSurveyTable extends Migration
      */
     public function up()
     {
-        Schema::create('survey', function (Blueprint $table) {
+        Schema::create('survey_answers', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('prsn_id')->references('id')->on('persons')->unsigned();
-            $table->string('title', 255);
-            $table->string('description', 1500)->nullable()->default(NULL);
-            $table->timestamp('deadline');
-            $table->date('in_calendar');
+            $table->integer('survey_id')->references('id')->on('surveys')->unsigned();
+            $table->string('name', 255)->nullable()->default(NULL);
+            $table->integer('club_id')->references('id')->on('clubs')->unsigned()->nullable();
+            $table->integer('order')->unsigned();
             $table->timestamps();
-
         });
     }
 
@@ -31,6 +31,6 @@ class CreateSurveyTable extends Migration
      */
     public function down()
     {
-        Schema::drop('survey');
+        Schema::drop('survey_answer');
     }
 }
