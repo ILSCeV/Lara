bfe390d30959d5bae4bfa714b2894bf323743766
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-06-01 16:14:25 +0200

    rename survey "prsn_id" into "creator_id"

--- database/migrations/2016_04_30_044140_create_survey_answers_table.php
+++ database/migrations/2016_04_30_044140_create_survey_answers_table.php
@@ -15,7 +15,7 @@ class CreateSurveyAnswersTable extends Migration
     {
         Schema::create('survey_answers', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('creator_id')->references('id')->on('persons')->unsigned();
+            $table->integer('creator_id')->references('id')->on('persons')->unsigned()->nullable();
             $table->integer('survey_id')->references('id')->on('surveys')->unsigned();
             $table->string('name', 255)->nullable()->default(NULL);
             $table->integer('club_id')->references('id')->on('clubs')->unsigned()->nullable();
