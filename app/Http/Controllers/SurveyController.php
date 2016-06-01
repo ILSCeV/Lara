f56873e4087666928a62d6106271ef3a51927a9c
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-06-01 15:48:58 +0200

    add authentication to edit, update and delete routes in surveys

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -18,6 +18,11 @@ use Input;
 
 class SurveyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('creatorSurvey', ['only' => ['edit', 'update', 'delete']]);
+    }
+
     public function index()
     {
 
