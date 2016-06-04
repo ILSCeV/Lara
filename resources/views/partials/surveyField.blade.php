79ee4fa792cf42dfd7244bc35754cdd63e6ee56e
Achim Bode <achim.bode@tu-ilmenau.de>
2016-06-04 13:07:10 +0200

    Clearing the input text after creating a new question

--- resources/views/partials/surveyField.blade.php
+++ resources/views/partials/surveyField.blade.php
@@ -86,17 +86,17 @@
                 <h4 id="reference" name="reference" class="heading-reference">Frage #1</h4>
 
                 <fieldset>
-                    <label class="question" for="question">Frage:</label>
+                    <label class="questions" for="question">Frage:</label>
 
-                    <input class="question" type="text" name="questions[]" id="question" value="">
+                    <input class="questions" type="text" name="questions[]" id="question" value="">
                 </fieldset>
 
 
                 <div style="visibility:hidden; display:none">
-                    <div id="new_passage"><table name="cloneTable">
+                    <div id="new_passage"><table id="new_passage" name="cloneTable">
                             <tr>
                                 <td>Antwortmöglichkeit</td>
-                                <td><input type="text" name="answer[]"></input></td>
+                                <td><input type="text" name="answer_option[][]"></input></td>
                                 <td class="helltab" rowspan="3">
                                     <a href="#" id="delete_button" onclick="javascript:remove_this(this); return false;">
                                          <i class="fa fa-trash" aria-hidden="true"></i></a>
