65a885ffadd3cfc6cbb1807ccd888495ce061f0c
Jan Mensch <jan.mensch@tu-ilmenau.de>
2016-06-15 14:59:50 +0200

    change div to textarea on click

--- public/js/surveyView-scripts.js
+++ public/js/surveyView-scripts.js
@@ -3,14 +3,18 @@
     Reaction to click on edit delete buttons
 */
 $(document).ready(function() {
+    //currently nothing
+});
 
-    alert("Hello! I am an alert box!!");
-
-    var docHeight = $(window).height();
-    var footerHeight = $('#footer').height();
-    var footerTop = $('#footer').position().top + footerHeight;
-
-    if (footerTop < docHeight) {
-        $('#footer').css('margin-top', (docHeight - footerTop) + 'px');
-    }
+//source: http://stackoverflow.com/questions/2441565/how-do-i-make-a-div-element-editable-like-a-textarea-when-i-click-it
+$("div").click(function() {
+    //save HTML within div
+    var divHtml = $(this).html(); // notice "this" instead of a specific #myDiv
+    //create dynamic textarea
+    var editableText = $("<textarea />");
+    //fill textarea with text of former div
+    editableText.val(divHtml);
+    //replace div with textarea
+    $(this).replaceWith(editableText);
+    //editableText.focus(); (optional) 
 });
