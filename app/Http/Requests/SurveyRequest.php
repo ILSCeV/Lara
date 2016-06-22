69a68a04840209023100734f9e85bdfad2f930b6
Markus Postler <markus.postler@tu-ilmenau.de>
2016-06-22 14:12:17 +0200

    make SurveyRequest

---
+++ app/Http/Requests/SurveyRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lara\Http\Requests;
+
+use Lara\Http\Requests\Request;
+
+class SurveyRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
