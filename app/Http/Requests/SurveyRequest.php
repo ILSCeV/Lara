f87c9c987fdc63325eb3d5ac35127eba004f1106
Markus Postler <markus.postler@tu-ilmenau.de>
2016-06-22 14:32:12 +0200

    added survey attributs to validation rules, temporary: authorization always true

--- app/Http/Requests/SurveyRequest.php
+++ app/Http/Requests/SurveyRequest.php
@@ -3,6 +3,7 @@
 namespace Lara\Http\Requests;
 
 use Lara\Http\Requests\Request;
+use Carbon\Carbon;
 
 class SurveyRequest extends Request
 {
@@ -13,7 +14,8 @@ class SurveyRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        //implement later: check if user has permission to create/edit -> has LDAP_Id?
+        return true;
     }
 
     /**
@@ -24,7 +26,12 @@ class SurveyRequest extends Request
     public function rules()
     {
         return [
-            //
+            'title' => 'string|required|size:255',
+            'description' => 'string|size:1500',
+            //'deadline' => 'date, after:Carbon::now' ??? deadline is dateTime not Date
+            'is_private' => 'boolean',
+            'is_anonymous' => 'boolean',
+            'show_results_after_voting' => 'boolean'
         ];
     }
 }
