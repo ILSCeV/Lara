55b6f2c0bab11ae8c6fc0101dfe081fed15f1416
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-06-24 14:06:23 +0200

    add missing variables to Revision_SurveyAnswer Model

--- app/Revision_SurveyAnswer.php
+++ app/Revision_SurveyAnswer.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Revision_SurveyAnswer extends Model
 {
+
+    public $timestamps = false;
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'revisions_survey_answers';
+
+    /**
+     * The database columns used by the model.
+     * This attributes are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array();
+
+
     /**
      * Get the corresponding Revision.
      *
