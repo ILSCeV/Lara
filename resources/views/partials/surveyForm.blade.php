cbc542d21de33482af22e10ca55aefb7a16a2a7f
Achim Bode <achim.bode@tu-ilmenau.de>
2016-06-25 23:38:38 +0200

    Fixed a bug concerning the button for deleting questions.

--- resources/views/partials/surveyForm.blade.php
+++ resources/views/partials/surveyForm.blade.php
@@ -559,7 +559,7 @@
                         </div>
 
                         <div class="col-md-1 col-sm-1 col-xs-2">
-                            <input id="button_del_question1" type="button" class="btn btn-small btn-danger fa fa-trash btnRemoveQuestion" name="button_del_question" value="&#xf1f8;">
+                            <input id="button_del_question1" type="button" class="btn btn-sm btn-danger fa fa-trash btnRemoveQuestion" name="button_del_question" value="&#xf1f8;">
                         </div>
 
 
