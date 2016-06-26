8174e03b947165054ffd364f7b4532b8d7c0d1d7
Lawu5163 a <Lawu5163 a>
2016-06-26 17:00:20 +0200

    Fix the right transfer of the datetime for editing the survey NOT TESTED YET!

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -280,10 +280,12 @@ class SurveyController extends Controller
             $answer_options = $question->getAnswerOptions;
 
         // prepare correct date and time format to be used in forms for deadline
-        $time = strftime("%d-%m-%Y %H:%M:%S", strtotime($survey->deadline));
-
-
-        return view('editSurveyView', compact('survey', 'questions', 'answer_options', 'time'));
+        //old: $time = strftime("%d-%m-%Y %H:%M:%S", strtotime($survey->deadline));
+        //new:
+        $datetime = strftime("%d-%m-%Y %H:%M:%S", strtotime($survey->deadline));
+        $time = $datetime ->toTimeString();
+        $date = $datetime ->toDateString();
+        return view('editSurveyView', compact('survey', 'questions', 'answer_options', 'time', 'date'));
     }
 
     /**
