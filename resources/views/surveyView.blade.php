5e21eb239e2958291068ed7b949cb79066971ec9
Jan Mensch <jan.mensch@tu-ilmenau.de>
2016-06-28 01:52:50 +0200

    make buttons work on mobile

--- resources/views/surveyView.blade.php
+++ resources/views/surveyView.blade.php
@@ -45,125 +45,126 @@ $userCanEditDueToRole
     <br>
     <br>
 
-
-    <div class="panel">
-        <div id="change-history">
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered table-condensed table-responsive">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Club</th>
-                        @foreach($questions as $question)
-                            <th class="question">
-                                {{$question->question}}
-                                @if($question->is_required == 1)
-                                    *
-                                @endif
-                            </th>
+    <div id="change-history" class="table-responsive panel ">
+        <table class="table table-striped table-bordered table-condensed table-responsive">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Club</th>
+                @foreach($questions as $question)
+                    <th class="question">
+                        {{$question->question}}
+                        @if($question->is_required == 1)
+                            *
+                        @endif
+                    </th>
+                @endforeach
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($answers as $key => $answer)
+                <tr>
+                    <td>{{$answer->name}}</td>
+                    <td>
+                        @if($club = $clubs->find($answer->club_id))
+                            {{$club->clb_title}}
+                        @else
+                            kein Club
+                        @endif
+                    </td>
+                    @foreach($answer->getAnswerCells as $cell)
+                        <td class="singleAnswer">
+                            {{$cell->answer}}
+                        </td>
                         @endforeach
-                                <!--<th >
-
-                            <a
-                                    class="editButton btn btn-primary editRow"
-                                    data-toggle="tooltip"
-                                    data-placement="bottom">
+                        @if($userId == $answer->creator_id OR $userCanEditDueToRole)
+                                <!--Edid Delete Buttons-->
+                        <td class="tdButtons displayDesktop">
+                            <a href="#"
+                               class="editButton btn btn-primary displayDesktop"
+                               data-toggle="tooltip"
+                               data-placement="bottom">
                                 <i class="fa fa-pencil"></i>
                             </a>
                             <a
-                                    class="btn btn-default deleteRow"
-                                    data-toggle="tooltip"
-                                    data-placement="bottom"
-                                    data-method="delete"
-                                    data-token="{{csrf_token()}}"
-                                    rel="nofollow"
-                                    data-confirm="Möchtest Du diese Antwort wirklich löschen?">
+                               class="btn btn-default displayDesktop"
+                               data-toggle="tooltip"
+                               data-placement="bottom"
+                               data-method="delete"
+                               data-token="{{csrf_token()}}"
+                               rel="nofollow"
+                               data-confirm="Möchtest Du diese Antwort wirklich löschen?">
                                 <i class="fa fa-trash"></i>
                             </a>
-                        </th>-->
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($answers as $key => $answer)
-                        <tr>
-                            <td>{{$answer->name}}</td>
-                            <td>
-                                @if($club = $clubs->find($answer->club_id))
-                                    {{$club->clb_title}}
-                                @else
-                                    kein Club
-                                @endif
+                        </td>
+                        @else
+                            <td class="emptyNoButtons displayDesktop">
                             </td>
-                            @foreach($answer->getAnswerCells as $cell)
-                                <td class="singleAnswer">
-                                    {{$cell->answer}}
+                            @endif
+                            @if($userId == $answer->creator_id OR $userCanEditDueToRole)
+                                    <!--Edid Delete Buttons-->
+                            <td class="displayMobile ">
+                                <a href="#"
+                                   class="editButton btn btn-primary displayMobile"
+                                   data-toggle="tooltip"
+                                   data-placement="bottom">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                                <a href=""
+                                   class="btn btn-default displayMobile btn-trash"
+                                   data-toggle="tooltip"
+                                   data-placement="bottom"
+                                   data-method="delete"
+                                   data-token="{{csrf_token()}}"
+                                   rel="nofollow"
+                                   data-confirm="Möchtest Du diese Antwort wirklich löschen?">
+                                    <i class="fa fa-trash"></i>
+                                </a>
+                            </td>
+                            @else
+                                <td class="emptyNoButtons">
                                 </td>
-                                @endforeach
-                                @if($userId == $answer->creator_id OR $userCanEditDueToRole)
-                                        <!--Edid Delete Buttons-->
-                                <td class="tdButtons ">
-                                    <a
-                                            class="editButton btn btn-primary editRow"
-                                            data-toggle="tooltip"
-                                            data-placement="bottom">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                    <a href="{{$survey->id}}/answer/{{$answer->id}}"
-                                       class="btn btn-default deleteRow"
-                                       data-toggle="tooltip"
-                                       data-placement="bottom"
-                                       data-method="delete"
-                                       data-token="{{csrf_token()}}"
-                                       rel="nofollow"
-                                       data-confirm="Möchtest Du diese Antwort wirklich löschen?">
-                                        <i class="fa fa-trash"></i>
-                                    </a>
-                                </td>
-                                @else
-                                    <td class="emptyNoButtons tdButtons">
-                                    </td>
-                                @endif
-                        </tr>
-                    @endforeach
-                    </tbody>
-                    <!--
-                                        <tbody>
-                                        <tr>
-                                            <td>Jan</td>
-                                            <td>C</td>
-                                            <td>Antwort auf die Frage 1</td>
-                                            <td>ja</td>
-                                            <td>nein</td>
-                                            <td>eine ziemlich lange Antwort mit viel bla bla und etc.</td>
-                                            <td>A5</td>
-                                            <td class="tdButtons">Buttons</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Lars</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td class="tdButtons">Buttons</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Fridolin</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td>Table cell</td>
-                                            <td class="tdButtons">Buttons</td>
-                                        </tr>
-                                        </tbody>
-                        -->
-                </table>
-            </div>
-        </div>
+                            @endif
+                </tr>
+            @endforeach
+            </tbody>
+            <!--
+                                <tbody>
+                                <tr>
+                                    <td>Jan</td>
+                                    <td>C</td>
+                                    <td>Antwort auf die Frage 1</td>
+                                    <td>ja</td>
+                                    <td>nein</td>
+                                    <td>eine ziemlich lange Antwort mit viel bla bla und etc.</td>
+                                    <td>A5</td>
+                                    <td class="tdButtons">Buttons</td>
+                                </tr>
+                                <tr>
+                                    <td>Lars</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td class="tdButtons">Buttons</td>
+                                </tr>
+                                <tr>
+                                    <td>Fridolin</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td class="tdButtons">Buttons</td>
+                                </tr>
+                                </tbody>
+                -->
+        </table>
     </div>
+
 
 
 
