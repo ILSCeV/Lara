d84998066689a90493c60c2bdaccd0423e3f8651
Armin Balzer <Armin Balzer>
2016-07-01 11:27:39 +0200

    Overworking the difference between a user and a guest, because its not clear

--- resources/views/partials/weekCellSurvey.blade.php
+++ resources/views/partials/weekCellSurvey.blade.php
@@ -1,13 +1,14 @@
 &nbsp;
 @if(!Session::has('userId') AND $survey->is_private == 0)
 	{{-- check current session for user role && and the survey for private status--}}
+	{{-- no userId means this a guest account, so he gets blocked here--}}
     <div class="panel panel-warning">
 
-		{{--if so show a grey placeholder for the user--}}
+		{{--if so show a grey placeholder for the guest--}}
         <div class="panel dark-grey white-text" style="padding: 15px 15px 8px 15px;">
 			<h4 class="panel-title">
 					<i class="fa fa-bar-chart-o white-text"></i>
-					{{--and show him thats a private survey ( =Interne Umfrage in german)--}}
+					{{--and show him thats a private survey(=Interne Umfrage in german) only for users--}}
 					<span class="name">Interne Umfrage</span>
 			</h4>
 			<i class="fa fa-times" aria-hidden="true"></i>
@@ -18,19 +19,23 @@
             </div>
 	</div>
 @else
-	{{----}}
+	{{-- session has a valid user now or the survey is public--}}
+	{{-- BUG? sollte ein user keine userid haben also gäste können sie private umfragen sehen? @Alex Fro--}}
 	<div class="panel panel-warning">
-	<div class="panel panel-heading calendar-public-info white-text">
+
+		<div class="panel panel-heading calendar-public-info white-text">
 			<h4 class="panel-title">
+				{{-- provide a URL to the survey --}}
 				<a href="{{ URL::route('survey.show', $survey->id) }}">
 					<i class="fa fa-bar-chart-o white-text"></i>&nbsp;
+					{{-- instead of private survey show the actual titel of the survey --}}
 					<span class="name">{{ $survey->title }}</span>
 				</a>
 			</h4>
-
-		<i class="fa fa-times" aria-hidden="true"></i>
+			<i class="fa fa-times" aria-hidden="true"></i>
 			{{ utf8_encode(strftime("%a, %d. %b", strtotime($survey->deadline))) }}
-	</div>
+		</div>
+
 	<div class="panel panel-body no-padding">
 
 
@@ -38,6 +43,7 @@
                AND (Session::get('userGroup') == 'marketing'
                OR Session::get('userGroup') == 'clubleitung'
                OR Session::get('userGroup') == 'admin'))
+			{{-- gives a session from privileged users the option to hide the event--}}
 	<hr class="col-md-12 col-xs-12 top-padding no-margin no-padding">
 	<div class="padding-right-16 bottom-padding pull-right hidden-print">
 		<small><a href="#" class="hide-event">Ausblenden</a></small>
