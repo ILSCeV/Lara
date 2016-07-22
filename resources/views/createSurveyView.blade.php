e9994ae13d4dc28d7179a15165ddceef3dd5a726
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-07-22 12:25:05 +0200

    format code

--- resources/views/createSurveyView.blade.php
+++ resources/views/createSurveyView.blade.php
@@ -6,21 +6,14 @@
 
 @section('content')
     <div class="row">
-
-
-    {!! Form::open(array('action' => 'SurveyController@store')) !!}
-        @include('partials.surveyForm', ['submitButtonText' => 'Umfrage erstellen'])
-
-
-    &nbsp;
+        {!! Form::open(array('action' => 'SurveyController@store')) !!}
+            @include('partials.surveyForm', ['submitButtonText' => 'Umfrage erstellen'])&nbsp;
             <div class="form-group">
                 {!! Form::submit(Lang::get('mainLang.createSurvey'), ['class'=>'btn btn-primary']) !!}
                 &nbsp;&nbsp;&nbsp;&nbsp;<br class="visible-xs">
                 <br class="visible-xs">
                 <a href="javascript:history.back()" class="btn btn-default">{{ trans('mainLang.backWithoutChange') }}</a>
             </div>
-    {!! Form::close() !!}
-
-</div>
-
+        {!! Form::close() !!}
+    </div>
 @stop
--- resources/views/editSurveyView.blade.php
+++ resources/views/editSurveyView.blade.php
@@ -6,31 +6,26 @@
 
 @section('content')
     <div class="row">
-
-
-    {!! Form::model($survey, array('action' => ['SurveyController@update', $survey->id], 'method' => 'PATCH')) !!}
-        @include('partials.surveyForm', ['submitButtonText' => 'Umfrage ändern'])
-
-
-    &nbsp;
-    <div class="form-group">
-        {!! Form::submit(Lang::get('mainLang.editSurvey'), ['class'=>'btn btn-primary']) !!}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <br class="visible-xs">
-        <a href="javascript:history.back()" class="btn btn-default">{{ trans('mainLang.backWithoutChange') }}</a>
-        &nbsp;
-        <a href="/survey/{{$survey->id}}"
-           class="btn btn-default"
-           data-toggle="tooltip"
-           data-placement="bottom"
-           data-method="delete"
-           data-token="{{csrf_token()}}"
-           rel="nofollow"
-           data-confirm='{{ trans('mainLang.confirmDeleteSurvey',['title' => $survey->title]) }}'>
-            <i class="fa fa-trash"></i>
-        </a>
-    </div>
-
-    {!! Form::close() !!}
+        {!! Form::model($survey, array('action' => ['SurveyController@update', $survey->id], 'method' => 'PATCH')) !!}
+            @include('partials.surveyForm', ['submitButtonText' => 'Umfrage ändern'])
+            &nbsp;
+            <div class="form-group">
+                {!! Form::submit(Lang::get('mainLang.editSurvey'), ['class'=>'btn btn-primary']) !!}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <br class="visible-xs">
+                <a href="javascript:history.back()" class="btn btn-default">{{ trans('mainLang.backWithoutChange') }}</a>
+                &nbsp;
+                <a href="/survey/{{$survey->id}}"
+                   class="btn btn-default"
+                   data-toggle="tooltip"
+                   data-placement="bottom"
+                   data-method="delete"
+                   data-token="{{csrf_token()}}"
+                   rel="nofollow"
+                   data-confirm='{{ trans('mainLang.confirmDeleteSurvey',['title' => $survey->title]) }}'>
+                    <i class="fa fa-trash"></i>
+                </a>
+            </div>
+        {!! Form::close() !!}
     </div>
 @stop
