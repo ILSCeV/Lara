b96a1a4ffafaa362307d7ae975669a767fd7c574
4D44H <4d44h@protonmail.com>
2016-08-28 19:40:09 +0200

    Bugfix
    Whitespace was recognized as part of the link and was throwing an error - removed whitespace.

--- resources/views/monthView.blade.php
+++ resources/views/monthView.blade.php
@@ -117,14 +117,8 @@
                 @endif
                         <div class="cell10 padleft">
                             @if(Session::has('userGroup'))
-                                <a href="{{ Request::getBasePath() }}/
-                                            event/
-                                            {{ $date['year']}}/
-                                            {{ $date['month'] }}/
-                                            {{ strftime("%d", strtotime($i - $date['startDay']." day", $date['startStamp'])) }}/
-                                            0/
-                                            create">
-                                            {{$simpleDate}}
+                                <a href="{{ Request::getBasePath() }}/event/{{ $date['year']}}/{{ $date['month'] }}/{{ strftime("%d", strtotime($i - $date['startDay']." day", $date['startStamp'])) }}/0/create">
+                                    {{$simpleDate}}
                                 </a>
                             @else
                                 {{$simpleDate}}
