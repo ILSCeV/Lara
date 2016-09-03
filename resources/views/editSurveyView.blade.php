4cfbaa77e9317274f774c9876739f0a4a4a328bf
4D44H <4d44h@protonmail.com>
2016-09-03 16:39:03 +0200

    Added forgotten scripts (corrected)

--- resources/views/editSurveyView.blade.php
+++ resources/views/editSurveyView.blade.php
@@ -4,6 +4,14 @@
     {{ trans('mainLang.editSurvey') }}
 @stop
 
+@section('moreStylesheets')
+    <style>
+        .dropdown-toggle {
+            text-transform: capitalize;
+        }
+    </style>
+@stop
+
 @section('content')
     <div class="row">
         {!! Form::model($survey, array('action' => ['SurveyController@update', $survey->id], 'method' => 'PATCH')) !!}
@@ -28,4 +36,8 @@
             </div>
         {!! Form::close() !!}
     </div>
+@stop
+
+@section('moreScripts')
+    <script src="{{ asset('js/surveyEdit-Create-scripts.js') }}"></script>
 @stop
