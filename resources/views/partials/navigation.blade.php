f933df0aa201037e08838bcf934aafe41b752666
4D44H <4d44h@protonmail.com>
2016-11-06 19:55:24 +0100

    fix navigation

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -44,7 +44,7 @@
                 <span class="caret"></span>
               </a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ asset('/manage/jobtypes') }}">{{ trans('mainLang.manageJobType') }}</a></li>
+                <li><a href="{{ asset('jobtype') }}">{{ trans('mainLang.manageJobType') }}</a></li>
               </ul>
             </li>
             @endif
