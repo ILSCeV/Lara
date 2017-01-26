0998e926b54cf4c8a938f7652190dc7b7fef2183
Stefan Heyder <stefan.heyder@gmail.com>
2017-01-26 21:53:42 +0100

    [Statistics] Correct wording (Club -> Section)

--- resources/views/partials/statistics/amountOfShiftsDisplay.blade.php
+++ resources/views/partials/statistics/amountOfShiftsDisplay.blade.php
@@ -1,6 +1,6 @@
-<span data-toggle="tooltip" title="{{trans('mainLang.shiftsInOwnClub')}}">
+<span data-toggle="tooltip" title="{{trans('mainLang.shiftsInOwnSection')}}">
     {{$info->inOwnClub}}
 </span>
-<span data-toggle="tooltip" title="{{trans('mainLang.shiftsInOtherClubs')}}" style="color: lightgrey">
+<span data-toggle="tooltip" title="{{trans('mainLang.shiftsInOtherSection')}}" style="color: lightgrey">
     {{ $info->inOtherClubs > 0 ? '+ ' . $info->inOtherClubs : '' }}
 </span>
