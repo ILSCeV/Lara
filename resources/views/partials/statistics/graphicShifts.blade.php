77b819e4e0b2a12903d75672a12a0ccda54e2d45
4D44H <4d44h@protonmail.com>
2017-01-28 18:23:04 +0100

    [Statistics] Update color scheme

--- resources/views/partials/statistics/graphicShifts.blade.php
+++ resources/views/partials/statistics/graphicShifts.blade.php
@@ -5,11 +5,11 @@
          title="{{trans('mainLang.shiftsInOwnSection')}}"></div>
     {{-- Progress bar for shifts in other club (marked in grey) --}}
     <div class="progress-bar"
-         style="width: {{$info->shiftsPercentExtern}}%; background-color: lightgrey;"
+         style="width: {{$info->shiftsPercentExtern}}%; background-color: lightskyblue;"
          data-toggle="tooltip"
          title="{{trans('mainLang.shiftsInOtherSection')}}">
     </div>
     <div class="progress-bar"
-         style="width: {{100 - $info->shiftsPercentExtern - $info->shiftsPercentIntern}}%; background-color: lightblue;">
+         style="width: {{100 - $info->shiftsPercentExtern - $info->shiftsPercentIntern}}%; background-color: gainsboro;">
     </div>
 </div>
