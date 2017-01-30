e9071ca2650e1717ef83e7a0b21da6bb040a12e4
4D44H <4d44h@protonmail.com>
2017-01-30 23:47:48 +0100

    [Month] Fix conditions for scrolling
    Now checking if we are in month view and if this is current month with a today-marker present.
    If same function will be used in week view, the month-view-check may be removed.

--- public/js/vedst-scripts.js
+++ public/js/vedst-scripts.js
@@ -56,15 +56,18 @@ $('.languageSwitcher').find('a').click(function() {
 ////////////////
 
 
-// Scroll to current date/event if in mobile view
-if ($('.month-view-marker').length) 
+// Scroll to current date/event if in mobile view in current month
+$(document).ready(function() 
 {
-    if ($(window).width() < 978) 
+    // check if we are in month view and if the today-marker exists
+    if ($('#month-view-marker').length && $(".scroll-marker").length) 
     {
-        $('html, body').animate({ scrollTop: $(".scroll-marker").offset().top -80 }, 1000);
+        if ($(window).width() < 978) 
+        {
+            $('html, body').animate({ scrollTop: $(".scroll-marker").offset().top -80 }, 1000);
+        };
     };
-};
-
+});
 
 
 
