12efe63c80158e384d3979d0bccc03d25cdd7934
fabian <f.markert87@googlemail.com>
2017-02-02 02:28:39 +0100

    #209 format the code

--- app/Http/Controllers/IcalController.php
+++ app/Http/Controllers/IcalController.php
@@ -24,7 +24,7 @@ class IcalController extends Controller
     {
         $vCalendar = new Calendar('Events');
 
-        $events = ClubEvent::where("evnt_is_private","=",'0')
+        $events = ClubEvent::where("evnt_is_private", "=", '0')
             ->with('place')
             ->get();
         $vEvents = $events->map(function ($evt) {
@@ -54,17 +54,18 @@ class IcalController extends Controller
      * @param $club_id - your club id, for example 1970
      * @param $alarm - how many minutes you want to remind before the event
      */
-    public function userScheduleWithAlarm($club_id, $alarm) {
+    public function userScheduleWithAlarm($club_id, $alarm)
+    {
 
         $person = Person::where('prsn_ldap_id', '=', $club_id)->first();
 
         $vCalendar = new Calendar('Events');
-        $events = ScheduleEntry::where('prsn_id','=', $person->id)->with("schedule","schedule.event.place","schedule.event")->get();
+        $events = ScheduleEntry::where('prsn_id', '=', $person->id)->with("schedule", "schedule.event.place", "schedule.event")->get();
 
         $vEvents = $events->map(function ($evt) use ($alarm) {
             $schedule = $evt->schedule;
             $start_time = "";
-            if($schedule->event->evnt_time_start == $evt->entry_time_start){
+            if ($schedule->event->evnt_time_start == $evt->entry_time_start) {
                 $start_time = $schedule->schdl_time_preparation_start;
             } else {
                 $start_time = $evt->entry_time_start;
@@ -74,14 +75,14 @@ class IcalController extends Controller
             $start_date_time = \DateTime::createFromFormat(self::DATE_FORMAT, "" . $schedule->event->evnt_date_start . " " . $start_time);
             $stop_date_time = \DateTime::createFromFormat(self::DATE_FORMAT, "" . $schedule->event->evnt_date_end . " " . $evt->entry_time_end);
 
-            if($start_date_time!=false && $stop_date_time != false) {
+            if ($start_date_time != false && $stop_date_time != false) {
                 $vEvent->setDtStart($start_date_time);
                 $vEvent->setDtEnd($stop_date_time);
                 $vEvent->setSummary($schedule->event->evnt_title);
                 $vEvent->setDescription($schedule->event->evnt_public_info);
                 $place = $schedule->event->place->plc_title;
                 $vEvent->setLocation($place, $place);
-                if ( $alarm >0 && ($start_date_time > new \DateTime()) ) {
+                if ($alarm > 0 && ($start_date_time > new \DateTime())) {
                     $vAlarm = new Alarm();
                     $vAlarm->setAction(Alarm::ACTION_DISPLAY);
                     $vAlarm->setDescription($schedule->event->evnt_title);
