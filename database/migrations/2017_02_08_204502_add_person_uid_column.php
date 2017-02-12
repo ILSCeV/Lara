d0afc6a96a1e8fa9dadeb81f92aab4a7733fc348
Fabian Markert <f.markert87@googlemail.com>
2017-02-12 20:44:59 +0100

    #209 update uid for places

--- database/migrations/2017_02_08_204502_add_person_uid_column.php
+++ database/migrations/2017_02_08_204502_add_person_uid_column.php
@@ -1,7 +1,7 @@
 <?php
-use \Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Lara\Person;
 
 class AddPersonUidColumn extends Migration
@@ -18,10 +18,19 @@ class AddPersonUidColumn extends Migration
             $table->longText('prsn_uid');
         });
 
-        $persons = Person::all();
+        Schema::table('places', function (Blueprint $table){
+            $table->longText('place_uid');
+        });
+
+        $persons = Person::whereNotNull("prsn_ldap_id")->get();
         foreach ($persons as $person) {
             $person->prsn_uid = hash("sha512", uniqid());
             $person->save();
+        }
+        $places = \Lara\Place::all();
+        foreach ($places as $place){
+            $place->place_uid = hash("sha512", uniqid());
+            $place->save();
         }
     }
 
@@ -34,6 +43,9 @@ class AddPersonUidColumn extends Migration
     {
         Schema::table('persons', function (Blueprint $table) {
             $table->dropColumn('prsn_uid');
+        });
+        Schema::table('places', function (Blueprint $table) {
+            $table->dropColumn('place_uid');
         });
     }
 }
