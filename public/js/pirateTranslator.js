76f49afefad1e4bc731751e44153dba3017adc32
fabian <f.markert87@googlemail.com>
2017-03-31 23:35:19 +0200

    when pirate translation is active translate events with javascript

--- public/js/pirateTranslator.js
+++ public/js/pirateTranslator.js
@@ -3,14 +3,14 @@
  */
 $(window).load(
     function () {
-        console.log($('div.cal-event a'));
-
-        $('div.cal-event a').each(function (index, elem) {
+        var doTranslations = function (index, elem) {
             var translation = translate($(elem).text().trim());
-            console.log(translation);
             if(translation.indexOf('Translation necessary:') === -1){
                 $(elem).text(translation);
             }
-        });
+        };
+
+        $('div.cal-event a').each(doTranslations);
+        $('span.name').each(doTranslations);
     }
 );
