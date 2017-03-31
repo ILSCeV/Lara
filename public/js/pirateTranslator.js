5ebf6ca0171e829d65090c24f0889788c264a90c
fabian <f.markert87@googlemail.com>
2017-03-31 23:54:04 +0200

    add replaceAll function add more selectors

--- public/js/pirateTranslator.js
+++ public/js/pirateTranslator.js
@@ -1,16 +1,24 @@
 /**
  * Created by fabian on 31.03.17.
  */
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
 $(window).load(
     function () {
         var doTranslations = function (index, elem) {
-            var translation = translate($(elem).text().trim());
+            var oldText = $(elem).text().trim();
+            var translation = translate(oldText);
             if(translation.indexOf('Translation necessary:') === -1){
-                $(elem).text(translation);
+                $(elem).html($(elem).html().replaceAll(oldText, translation));
             }
         };
 
         $('div.cal-event a').each(doTranslations);
         $('span.name').each(doTranslations);
+        $('div.panelEventView .panel-title ').each(doTranslations);
     }
 );
