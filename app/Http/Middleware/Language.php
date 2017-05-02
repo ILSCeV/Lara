1e90969799c671604cad9d679446ddc2d241fe91
fabian <f.markert87@googlemail.com>
2017-05-02 23:40:16 +0200

    [2] iCal: google calendar can't fetch URL: add default value if header is not set and add log output

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -6,6 +6,7 @@ use App;
 use Application;
 use Closure;
 use Config;
+use Log;
 use Redirector;
 use Session;
 
@@ -14,19 +15,19 @@ class Language
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)    {
-        
+    public function handle($request, Closure $next)
+    {
+        Log::debug("".$request);
         if (Session::has('applocale') AND array_key_exists(Session::get('applocale'), Config::get('languages'))) {
             App::setLocale(Session::get('applocale'));
-        }
-        else {
+        } else {
             //Detect Browser Prefered language
-            $locale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
-            if(array_key_exists($locale,Config::get('languages'))){
+            $locale = substr($request->server('HTTP_ACCEPT_LANGUAGE', "de"), 0, 2);
+            if (isset($locale) && array_key_exists($locale, Config::get('languages'))) {
                 App::setLocale($locale);
                 Session::put('applocale', $locale);
             } else {
@@ -34,6 +35,7 @@ class Language
                 App::setLocale(Config::get('app.locale'));
             }
         }
+        
         return $next($request);
     }
 }
