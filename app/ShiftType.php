fe987811c059739744dc4b66676a355732daa570
Stefan Heyder <stefan.heyder@gmail.com>
2017-05-08 21:36:01 +0200

    Add new models
    We add new models (Shift for ScheduleEntry and ShiftType for Jobtype).
    These names are more accurate for what the model actually does.

---
+++ app/ShiftType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lara;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShiftType extends Model
+{
+    protected $table = 'jobtypes';
+
+    protected $fillable = [
+        'jbtyp_title',
+        'jbtyp_time_start',
+        'jbtyp_time_end',
+        'jbtyp_needs_preparation',
+        'jbtyp_statistical_weight',
+        'jbtyp_is_archived'
+    ];
+
+    /**
+     * Get the corresponding schedule entries.
+     * Looks up in table schedule_entries for entries, which have the same jbtyp_id like id of Jobtype instance.
+     *
+     * @return \vendor\laravel\framework\src\Illuminate\Database\Eloquent\Relations\HasMany of type ClubEvent
+     */
+    public function getJob(){
+        return $this->hasMany('Lara\ScheduleEntry', 'jbtyp_id', 'id');
+    }
+
+    public function title() {
+        return $this->jbtyp_title;
+    }
+
+    public function start() {
+        return $this->jbtyp_time_start;
+    }
+
+    public function end() {
+        return $this->jbtyp_time_end;
+    }
+
+    public function needsPreparation() {
+        return $this->jbtyp_needs_preparation;
+    }
+
+    public function weight() {
+        return $this->jbtyp_statistical_weight;
+    }
+
+    public function isArchived() {
+        return $this->jbtyp_is_archived;
+    }
+}
