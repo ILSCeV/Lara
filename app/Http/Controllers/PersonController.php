fff011b39e2f36743bf1e356de530404a5d26818
4D44H <4d44h@protonmail.com>
2017-05-22 00:36:04 +0200

    Update Persons with latest LDAP status
    First iteration: on calling the /ldapsync route the function will look for every person with an LDAP id in Lara, then get their current info from LDAP and overwrite Lara Persons with data from LDAP.
    ToDo: scheduling, logging.
    This is a deveopment version based on current master branch under Laravel 5.3

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -10,6 +10,8 @@ use Session;
 use Config;
 use Redirect;
 
+use Lara\Person;
+
 use Lara\Http\Requests;
 use Lara\Http\Controllers\Controller;
 
@@ -111,5 +113,114 @@ class PersonController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+    /** 
+     * Updates status of each Person saved in Lara with the latest state from LDAP
+	 * Main purpose: data is usually updated when a member logs in. This function targets updates for members who stop visiting Lara.
+     */
+    public function LDAPSync() 
+    {
+    	if (Session::get('userGroup') == 'admin') {
+
+            // get a list of all persons saved in Lara
+    		$persons = Person::whereNotNull('prsn_ldap_id')->get();
+
+            // start counting time before processing every person
+            $counterStart = microtime(true);
+
+// CONNECTING TO LDAP SERVER
+
+            $ldapConn = ldap_connect(Config::get('bcLDAP.server'), Config::get('bcLDAP.port'));
+
+            // Set some ldap options for talking to AD
+            // LDAP_OPT_PROTOCOL_VERSION: LDAP protocol version
+            ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            // LDAP_OPT_REFERRALS: Specifies whether to automatically follow referrals returned by the LDAP server
+            ldap_set_option($ldapConn, LDAP_OPT_REFERRALS, 0);
+
+            // Bind as a domain admin
+            $ldap_bind = ldap_bind($ldapConn,
+                Config::get('bcLDAP.admin-username'),
+                Config::get('bcLDAP.admin-password'));
+
+// STARTING THE UPDATE
+
+    		foreach ($persons as $person) {
+
+// AUTHENTICATING BC-CLUB
+
+                // Search for a bc-Club user with the uid number entered
+                $search = ldap_search($ldapConn,
+                    Config::get('bcLDAP.bc-club-ou') .
+                    Config::get('bcLDAP.base-dn'),
+                    '(uid=' . $person->prsn_ldap_id . ')');
+
+                $info = ldap_get_entries($ldapConn, $search);
+
+// AUTHENTICATING BC-CAFE
+
+                // If no such user found in the bc-Club - check bc-Café next.
+                if ($info['count'] === 0) {
+
+                    // Search for a Café-user with the uid number entered
+                    $search = ldap_search($ldapConn,
+                        Config::get('bcLDAP.bc-cafe-ou') .
+                        Config::get('bcLDAP.base-dn'),
+                        '(uid=' . $person->prsn_ldap_id . ')');
+
+                    $info = ldap_get_entries($ldapConn, $search);
+                }
+
+// HANDLING ERRORS
+
+                // If no match found in all clubs - throw an error and quit
+                if ($info['count'] === 0) {
+                    ldap_unbind($ldapConn);
+
+                    Session::put('message', Config::get('messages_de.uid-not-found'));
+                    Session::put('msgType', 'danger');
+
+                    Log::info('Auth fail: wrong userID given (username: ' . Input::get('username') . ').');
+
+                    return Redirect::back();
+                }               
+
+// GETTING USER CREDENTIALS
+
+                // Get user nickname if it exists or first name instead
+                $userName = (!empty($info[0]['mozillanickname'][0])) ?
+                    $info[0]['mozillanickname'][0] :
+                    $info[0]['givenname'][0];
+
+                // Get user active status
+                $userStatus = $info[0]['ilscstate'][0];
+
+// UPDATE AND SAVE CHANGES
+
+                print_r("Lara: " .  $person->id . " - " . $person->prsn_ldap_id . " - " . $person->prsn_name . " - " . $person->prsn_status .  "<br>");
+                print_r("LDAP: " . $info[0]['uidnumber'][0] . " - " . $userName . " - " . $userStatus . "<br>" . "<br>");
+                
+                $person->prsn_name = $userName;
+                $person->prsn_status = $userStatus;
+
+                $person->save();
+    		}
+
+// FINISH UPDATE
+
+            ldap_unbind($ldapConn);
+
+            $counterEnd = microtime(true);
+
+            // report update time
+            $response = ($counterEnd - $counterStart) . " sec";
+
+            dd($response);
+
+    	} else {
+            dd("admin only");
+        }
     }
 }
