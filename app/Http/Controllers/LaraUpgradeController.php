df68d545d41daa92f48524f2118e1ef7c7ea7bc6
fabian <f.markert87@googlemail.com>
2017-06-11 21:14:56 +0200

    add upgrade command to controller

--- app/Http/Controllers/LaraUpgradeController.php
+++ app/Http/Controllers/LaraUpgradeController.php
@@ -17,6 +17,17 @@ class LaraUpgradeController extends Controller
         if(!Utilities::requirePermission('admin')){
             return \Redirect::back($status = 403);
         }
-        return \View::make("upgrade");
+    
+        $cmd = "php artisan lara:update &";
+    
+        $descriptorspec = array(
+            0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+            1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+            2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+        );
+        
+        proc_open($cmd, $descriptorspec, $pipes, realpath('../'), array());
+        
+        return \Redirect::back();
     }
 }
