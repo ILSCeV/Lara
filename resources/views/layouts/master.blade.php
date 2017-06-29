7ea371918b19f543cbbb5f2f1cff673e7f8a0092
Stefan Heyder <stefan.heyder@gmail.com>
2017-06-29 20:41:54 +0200

    Simplify gitrevision import

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,11 +49,9 @@
                 <small><a href="mailto:maxim.drachinskiy@bc-studentenclub.de"> {{ trans('mainLang.notWorkingMail',['Name' => 'Maxim']) }} </a></small>
             </span>
             <span class="col-xs-12 col-sm-12 col-md-4 text-dark-grey" style="text-align: center;">
-                @if(File::exists("gitrevision.txt"))
-                    <small>{{File::get("gitrevision.txt")}}</small>
-                @else
-                    <small>&nbsp;</small>
-                @endif
+                <small>
+                    {{ File::exists("gitrevision.txt") ? File::get("gitrevision.txt") : "&nbsp;" }}
+                </small>
             </span>
             <span class="col-xs-12 col-sm-12 col-md-4 text-dark-grey" style="text-align: center;">
                 <small><a href="https://github.com/ILSCeV/Lara">{{ trans('mainLang.moreInfosProjectsite') }}</a>.
