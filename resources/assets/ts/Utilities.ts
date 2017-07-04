3016d165731b5e5549f18aa9860f0ce53f94690e
Stefan Heyder <stefan.heyder@gmail.com>
2017-07-04 23:05:25 +0200

    Remove obsolete function

--- resources/assets/ts/Utilities.ts
+++ resources/assets/ts/Utilities.ts
@@ -7,16 +7,6 @@ enum Club {
     BiClub
 }
 
-function getIdOfClub (club: string): Club {
-    switch (club) {
-        case "bc-Club":
-            return Club.BcClub;
-        case "bc-Café":
-            return Club.BcCafe;
-    }
-    return Club.None;
-}
-
 function showErrorModal(message: string) {
     $("#errorModal").modal("show");
     $("#errorModal").find(".modal-body").html(message);
