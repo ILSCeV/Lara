3ec6fd98996bd6d3b077caa7694940f1b0eab6da
Fabian Markert <f.markert87@googlemail.com>
2017-08-20 15:23:01 +0200

    section management controller createt

---
+++ app/Http/Controllers/SectionManagementController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lara\Http\Controllers;
+
+class SectionManagementController extends Controller
+{
+    public function create()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function view()
+    {
+    }
+}
