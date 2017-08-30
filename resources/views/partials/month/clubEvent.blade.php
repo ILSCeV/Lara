662bd245d7b7d67fd833273c9679b3af17fd6e74
Stefan Heyder <stefan.heyder@gmail.com>
2017-08-30 16:53:40 +0200

    Fix typo, caused filtering to not work

--- resources/views/partials/month/clubEvent.blade.php
+++ resources/views/partials/month/clubEvent.blade.php
@@ -1,6 +1,6 @@
 @if($clubEvent->evnt_date_start === date("Y-m-d", $weekDay->getTimestamp()))
     {{-- Workaround for older events withou evnt_show_to_club : use corresponding section, otherwise use all saved sections --}}
-    <div class="word-break section_filter {{ empty($clubEvent->evnt_show_to_club) ? $clubEvent->section->title : join(" " ,json_decode($clubEvent->evnt_show_to_club)) }}">
+    <div class="word-break section-filter {{ empty($clubEvent->evnt_show_to_club) ? $clubEvent->section->title : join(" " ,json_decode($clubEvent->evnt_show_to_club)) }}">
         <div class="{{ Lara\EventStyler::classesForEvent($clubEvent) }}">
             @include("partials/month/clubEventInner")
         </div>
