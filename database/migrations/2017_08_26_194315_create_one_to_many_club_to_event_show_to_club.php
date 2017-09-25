0dfd960a4190f780726a11b5d8d70fc9af1f07bd
fabian <f.markert87@googlemail.com>
2017-09-26 00:56:48 +0200

    implement rollback

--- database/migrations/2017_08_26_194315_create_one_to_many_club_to_event_show_to_club.php
+++ database/migrations/2017_08_26_194315_create_one_to_many_club_to_event_show_to_club.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Lara\Club;
 use Lara\ClubEvent;
 use Lara\EventsSections;
 use Lara\Section;
@@ -26,22 +25,31 @@ class CreateOneToManyClubToEventShowToClub extends Migration
             $table->foreign('club_event_id')->references('id')->on('club_events')->onDelete('cascade');
         });
         
-        $clubEvents = DB::table('club_events')->select('club_events.id','club_events.evnt_show_to_club','sections.title')->join('sections','club_events.plc_id','=','sections.id')->get();
+        $clubEvents = DB::table('club_events')->select('club_events.id', 'club_events.evnt_show_to_club',
+            'sections.title')->join('sections', 'club_events.plc_id', '=', 'sections.id')->get();
         
         /* @var $sections \Illuminate\Support\Collection */
         $sections = Section::all();
         
         /* @var $clubEvent ClubEvent */
-        $clubEvents->each(function($clubEvent) use ($sections) {
-           $showToClubs = json_decode($clubEvent->evnt_show_to_club);
-           if(is_null($showToClubs)){
-               $showToClubs=[$clubEvent->title];
-           }
-           return $sections->filter(function($section) use ($showToClubs){
-             return in_array($section->title, $showToClubs);
-            })->each(function($section) use ($clubEvent) {
-              DB::table('club_event_section')->insert(array('club_event_id' => $clubEvent->id, 'section_id'=>$section->id));
-           });
+        $clubEvents->each(function ($clubEvent) use ($sections) {
+            $showToClubs = json_decode($clubEvent->evnt_show_to_club);
+            if (is_null($showToClubs)) {
+                $showToClubs = [$clubEvent->title];
+            }
+            
+            return $sections->filter(function ($section) use ($showToClubs) {
+                return in_array($section->title, $showToClubs);
+            })->each(function ($section) use ($clubEvent) {
+                DB::table('club_event_section')->insert([
+                    'club_event_id' => $clubEvent->id,
+                    'section_id'    => $section->id,
+                ]);
+            });
+        });
+    
+        Schema::table('club_events', function (Blueprint $table) {
+            $table->dropColumn('evnt_show_to_club');
         });
     }
     
@@ -52,6 +60,23 @@ class CreateOneToManyClubToEventShowToClub extends Migration
      */
     public function down()
     {
+        Schema::table('club_events', function (Blueprint $table) {
+            $table->text('evnt_show_to_club')->after('plc_id');
+        });
+    
+        $eventsWithSection = DB::table('club_events')->select('club_events.id', 'sections.title')
+            ->join('club_event_section','club_event_id','=','club_events.id')
+            ->join('sections', 'club_event_section.section_id', '=', 'sections.id')
+            ->get();
+        $eventShowToSection = [];
+        foreach ($eventsWithSection as $eventWithSeciton){
+            $eventShowToSection[$eventWithSeciton->id][]=$eventWithSeciton->title;
+        }
+    
+        foreach ($eventShowToSection as $id => $sections){
+            DB::table('club_events')->where('id', $id)->update(['evnt_show_to_club' => json_encode($sections)]);
+        }
+        
         Schema::drop('club_event_section');
     }
 }
