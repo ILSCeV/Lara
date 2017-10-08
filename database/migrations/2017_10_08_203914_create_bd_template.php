8c820bb3188b57647fd9227e56c2b91883ba578f
fabian <f.markert87@googlemail.com>
2017-10-08 22:56:46 +0200

    add template for bd club

---
+++ database/migrations/2017_10_08_203914_create_bd_template.php
@@ -0,0 +1,111 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Lara\Schedule;
+use Lara\Shift;
+use Lara\ShiftType;
+
+class CreateBdTemplate extends Migration
+{
+    const BD_TEMPLATE_NAME = 'BD Template';
+    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        /* @var $avTemplate ShiftType */
+        $avTemplate = $this->createShiftType('AV');
+        /* @var $discoTemplate ShiftType */
+        $discoTemplate = $this->createShiftType('Disco');
+        $discoTemplate->title = 'Musik';
+        /* @var $tresenTemplate ShiftType */
+        $tresenTemplate = $this->createShiftType('Tresen');
+        $tresenTemplate->title = 'Bier';
+        /* @var $barTemplate ShiftType */
+        $barTemplate = $this->createShiftType('Bar');
+        /* @var $einlassTemplate ShiftType */
+        $einlassTemplate = $this->createShiftType('Einlass');
+        
+        /* @var $shiftType ShiftType */
+        $shiftTypes = collect([$avTemplate, $discoTemplate, $tresenTemplate, $barTemplate, $einlassTemplate]);
+        foreach ($shiftTypes as $shiftType) {
+            $shiftType->save();
+        }
+        
+        $template = new Schedule();
+        $template->fill([
+            'schdl_title'                  => self::BD_TEMPLATE_NAME,
+            'schdl_time_preparation_start' => '20:00:00',
+            'schdl_is_template'            => true,
+            'entry_revisions'              => '',
+        ]);
+        $template->save();
+        /* @var $shiftType ShiftType */
+        $shifts = $shiftTypes->flatMap(function ($shiftType) use ($template) {
+            $shiftCount = $shiftType->title == 'AV' || $shiftType->title == 'Musik' ? 1 : 2;
+            return collect(range(1, $shiftCount))->map(function ($sc) use ($shiftType, $template) {
+                $shift = new Shift();
+                $shift->fill([
+                    'schedule_id'        => $template->id,
+                    'shifttype_id'       => $shiftType->id,
+                    'start'              => $shiftType->start,
+                    'end'                => $shiftType->end,
+                    'statistical_weight' => $shiftType->statistical_weight,
+                ]);
+                return $shift;
+            });
+        });
+        foreach ($shifts as $shift){
+            $shift->save();
+        }
+    }
+    
+    /** finds and return a new shifttype
+     * @param $shiftName shifttype name
+     * @return ShiftType
+     */
+    private function createShiftType($shiftName)
+    {
+        $shiftTypeStart = '20:00:00';
+        $shiftTypeEnd = '01:00:00';
+        $shiftTimeRange = ['start' => $shiftTypeStart, 'end' => $shiftTypeEnd];
+        /* @var $shiftType ShiftType */
+        $shiftType = ShiftType::where('title', '=', $shiftName)->first()->replicate();
+        $shiftType->fill($shiftTimeRange);
+        
+        return $shiftType;
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        /* @var $template Schedule */
+        $template = Schedule::where('schdl_title','=',self::BD_TEMPLATE_NAME)->first();
+        $shifts = $template->shifts;
+        foreach ($shifts as $shift){
+            $shift->delete();
+        }
+        $template->delete();
+        
+        $shiftTypeStart = '20:00:00';
+        $shiftTypeEnd = '01:00:00';
+        
+        $shiftTypeNames = ['AV', 'Disco', 'Tresen', 'Bar', 'Einlass'];
+        $shiftTypes = ShiftType::whereIn('title', $shiftTypeNames)
+            ->where('start', '=', $shiftTypeStart)
+            ->where('stop', '=', $shiftTypeEnd)->get();
+        /* @var $shiftType ShiftType */
+        foreach ($shiftTypes as $shiftType) {
+            $shiftType->delete();
+        }
+    }
+}
