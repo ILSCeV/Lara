594de1911d64506a34638078a659f36eba18b8bb
fabian <f.markert87@googlemail.com>
2017-11-01 19:44:59 +0100

    fix Indentation

--- app/ClubEvent.php
+++ app/ClubEvent.php
@@ -9,6 +9,7 @@
 namespace Lara;
 
 use Illuminate\Database\Eloquent\Model;
+
 /**
  * @property int evnt_type
  * @property string evnt_title
@@ -27,72 +28,77 @@ use Illuminate\Database\Eloquent\Model;
  */
 class ClubEvent extends Model
 {
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var $table string
-	 */
-	protected $table = 'club_events';
-	
-	/**
-	 * The database columns used by the model.
-	 * This attributes are mass assignable.
-	 *
-	 * @var $fillable array
-	 */
-	protected $fillable = array('evnt_type',			// 0 -> default -> "normales Programm"
-														// 1 -> info only
-														// 2 -> highlight / special
-														// 3 -> live band / DJ / reading
-														// 4 -> internal event
-														// 5 -> private party -> "Nutzung"
-														// 6 -> cleaning -> "Fluten"
-														// 7 -> flyer / poster
-														// 8 -> tickets -> "Vorverkauf"
-														// 9 -> internal task -> everything else
-								'evnt_title',
-								'evnt_subtitle',
-								'plc_id',
-								'evnt_date_start',
-								'evnt_date_end',
-								'evnt_time_start',
-								'evnt_time_end',
-								'evnt_public_info',
-								'evnt_private_details',
-								'evnt_is_private',
-								'evnt_is_published',
-                                'external_id',
-                                'price_tickets_normal',
-                                'price_tickets_external',
-                                'price_normal',
-                                'price_external',
-                                'facebook_done',
-                                'event_url');
-
-	/**
-	 * Get the corresponding section.
-	 * Looks up in table sections for that entry, which has the same id like plc_id of ClubEvent instance.
-	 *
-	 * @return \vendor\laravel\framework\src\Illuminate\Database\Eloquent\Relations\BelongsTo of type Section
-	 */
-	public function section() {
+    /**
+     * The database table used by the model.
+     *
+     * @var $table string
+     */
+    protected $table = 'club_events';
+    
+    /**
+     * The database columns used by the model.
+     * This attributes are mass assignable.
+     *
+     * @var $fillable array
+     */
+    protected $fillable = [
+        'evnt_type',            // 0 -> default -> "normales Programm"
+                                // 1 -> info only
+                                // 2 -> highlight / special
+                                // 3 -> live band / DJ / reading
+                                // 4 -> internal event
+                                // 5 -> private party -> "Nutzung"
+                                // 6 -> cleaning -> "Fluten"
+                                // 7 -> flyer / poster
+                                // 8 -> tickets -> "Vorverkauf"
+                                // 9 -> internal task -> everything else
+        'evnt_title',
+        'evnt_subtitle',
+        'plc_id',
+        'evnt_date_start',
+        'evnt_date_end',
+        'evnt_time_start',
+        'evnt_time_end',
+        'evnt_public_info',
+        'evnt_private_details',
+        'evnt_is_private',
+        'evnt_is_published',
+        'external_id',
+        'price_tickets_normal',
+        'price_tickets_external',
+        'price_normal',
+        'price_external',
+        'facebook_done',
+        'event_url',
+    ];
+    
+    /**
+     * Get the corresponding section.
+     * Looks up in table sections for that entry, which has the same id like plc_id of ClubEvent instance.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo|Section
+     */
+    public function section()
+    {
         return $this->belongsTo('Lara\Section', 'plc_id', 'id');
     }
-
-	/**
-	 * Get the corresponding schedule.
-	 * Looks up in table schedules for that entry, which has the same evnt_id like id of ClubEvent instance.
-	 *
-	 * @return \vendor\laravel\framework\src\Illuminate\Database\Eloquent\Relations\HasOne of type Schedule
-	 */
-	public function getSchedule() {
-		return $this->hasOne('Lara\Schedule', 'evnt_id', 'id');
-	}
-
-	public function schedule() {
+    
+    /**
+     * Get the corresponding schedule.
+     * Looks up in table schedules for that entry, which has the same evnt_id like id of ClubEvent instance.
+     *
+     * @return \vendor\laravel\framework\src\Illuminate\Database\Eloquent\Relations\HasOne of type Schedule
+     */
+    public function getSchedule()
+    {
         return $this->hasOne('Lara\Schedule', 'evnt_id', 'id');
     }
-
+    
+    public function schedule()
+    {
+        return $this->hasOne('Lara\Schedule', 'evnt_id', 'id');
+    }
+    
     /**
      * Check if the signed in user has a shift in the current event
      * Looks up in table Shifts for entries, which has the same scheduleId like id of ClubEvent instance.
@@ -100,27 +106,32 @@ class ClubEvent extends Model
      *
      * @return Boolean
      */
-    public function hasShift($scheduleId, $userId) {
-
+    public function hasShift($scheduleId, $userId)
+    {
+        
         $shifts = Shift::where('schedule_id', '=', $scheduleId)->with('getPerson')->get();
-        foreach($shifts as $shift) {
-            if ( isset($shift->getPerson->prsn_ldap_id) && $shift->getPerson->prsn_ldap_id ==  $userId){
+        foreach ($shifts as $shift) {
+            if (isset($shift->getPerson->prsn_ldap_id) && $shift->getPerson->prsn_ldap_id == $userId) {
                 return true;
             }
         }
+        
         return false;
     }
-
-	public function shifts() {
-		return $this->hasManyThrough('Lara\Shift', 'Lara\Schedule', 'evnt_id', 'schedule_id', 'id');
-	}
-	
-	public function showToSection() {
+    
+    public function shifts()
+    {
+        return $this->hasManyThrough('Lara\Shift', 'Lara\Schedule', 'evnt_id', 'schedule_id', 'id');
+    }
+    
+    public function showToSection()
+    {
         return $this->belongsToMany('Lara\Section');
     }
     
-    public function showToSectionNames() {
-        return $this->showToSection()->get()->map(function($section) {
+    public function showToSectionNames()
+    {
+        return $this->showToSection()->get()->map(function ($section) {
             return $section->title;
         })->toArray();
     }
