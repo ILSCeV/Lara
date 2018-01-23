9cae32856cf7476a543a3a49455decafb23866e7
fabian <f.markert87@googlemail.com>
2018-01-23 22:43:27 +0100

    finished up migration template table will be created template column will be droped

--- database/migrations/2017_12_29_152706_create_templates_table.php
+++ database/migrations/2017_12_29_152706_create_templates_table.php
@@ -3,12 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Lara\Club;
 use Lara\Schedule;
+use Lara\Section;
 use Lara\Shift;
 
 class CreateTemplatesTable extends Migration
 {
     const BD_TEMPLATE_NAME = 'BD Template';
+
+    const BD_SECTION_NAME = 'bd-Club';
 
     /**
      * Run the migrations.
@@ -49,55 +53,88 @@ class CreateTemplatesTable extends Migration
             $table->foreign('section_id')->references('id')->on('sections')->onDelete('cascade');
         });
 
-        $templates = Schedule::where('schdl_is_template','=','1')->where('schdl_title','!=',self::BD_TEMPLATE_NAME)->get();
-        $templates->map(function (Schedule $template){
+        DB::table('schedules')->where('schdl_title', '=', self::BD_TEMPLATE_NAME)
+            ->update(['schdl_is_template' => '1']);
+
+        $bdSection = Section::where('title', '=', 'bd-Club')->first();
+
+        $templates = Schedule::where('schdl_is_template', '=', '1')->get();
+        $templates->map(function (Schedule $template) use ($bdSection) {
 
             // get template data
-            $shifts     = $template->shifts()
+            $shifts = $template->shifts()
                 ->with('type')
                 ->orderByRaw('position IS NULL, position ASC, id ASC')
                 ->get();
 
-            $title      = $template->schdl_title;
-            $subtitle   = $template->getClubEvent->evnt_subtitle;
-            $type       = $template->getClubEvent->evnt_type;
-            $section    = $template->getClubEvent->section;
-            $filter     = $template->getClubEvent->showToSection()->get();
-            $dv         = $template->schdl_time_preparation_start;
-            $timeStart  = $template->getClubEvent->evnt_time_start;
-            $timeEnd    = $template->getClubEvent->evnt_time_end;
-            $info       = $template->getClubEvent->evnt_public_info;
-            $details    = $template->getClubEvent->evnt_private_details;
-            $private    = $template->getClubEvent->evnt_is_private;
+            $event = $template->getClubEvent;
+            if (is_null($event)) {
+                $event = new \Lara\ClubEvent();
+                $event->evnt_subtitle = "";
+                $event->evnt_type = 0;
+                $event->evnt_time_start = '21:00:00';
+                $event->evnt_time_end = '01:00:00';
+                $event->evnt_public_info = '';
+                $event->evnt_private_details = '';
+                $event->evnt_is_private = false;
+                $event->evnt_is_published = true;
+            }
+            $title = $template->schdl_title;
+            $subtitle = $event->evnt_subtitle;
+            $type = $event->evnt_type;
+
+            if ($title != self::BD_TEMPLATE_NAME) {
+                $filter = $event->showToSection()->get();
+                $section = $event->section;
+            } else {
+                $section = Section::where('title', '=', self::BD_SECTION_NAME)->first();
+                if (is_null($section)) {
+                    $section = new Section();
+                    $section->title = self::BD_SECTION_NAME;
+                    $section->section_uid = hash("sha512", uniqid());
+                    $section->save();
+                    $club = new Club();
+                    $club->clb_title = $section->title;
+                    $club->save();
+                }
+                $filter = collect([]);
+            }
+            $dv = $template->schdl_time_preparation_start;
+            $timeStart = $event->evnt_time_start;
+            $timeEnd = $event->evnt_time_end;
+            $info = $event->evnt_public_info;
+            $details = $event->evnt_private_details;
+            $private = $event->evnt_is_private;
 
             $result = new \Lara\Template();
             $result->fill([
                 'title' => $title,
                 'subtitle' => $subtitle,
                 'type' => $type,
-                'section_id'=> $section->id,
-                'time_preparation_start'=>$dv,
-                'time_start'=> $timeStart,
-                'time_end'=>$timeEnd,
-                'public_info'=> $info,
-                'private_details'=>$details,
-                'is_private'=>$private
+                'section_id' => $section->id,
+                'time_preparation_start' => $dv,
+                'time_start' => $timeStart,
+                'time_end' => $timeEnd,
+                'public_info' => $info,
+                'private_details' => $details,
+                'is_private' => $private
             ]);
             $result->save();
 
-            $result->shifts()->sync($shifts->map(function (Shift $shift){
+            $result->shifts()->sync($shifts->map(function (Shift $shift) {
                 return $shift->id;
             })->toArray());
-            $result->showToSection()->sync($filter->map(function(\Lara\Section $section){
+            $result->showToSection()->sync($filter->map(function (Section $section) {
                 return $section->id;
             })->toArray());
             $result->save();
             return $result;
         });
 
-        Schema::table("schedules", function(Blueprint $table) {
+        Schema::table("schedules", function (Blueprint $table) {
             $table->dropColumn("schdl_is_template");
         });
+
 
     }
 
@@ -108,14 +145,16 @@ class CreateTemplatesTable extends Migration
      */
     public function down()
     {
-        Schema::table("schedules", function(Blueprint $table) {
+        Schema::table("schedules", function (Blueprint $table) {
             $table->boolean("schdl_is_template");
         });
         $templateTitles = DB::table("templates")->select("title")->get()
-            ->map(function ($tt){return $tt->title;})->toArray();
-        DB::table("schedules")->update(["schdl_is_template"=>"0"]);
-        DB::table("schedules")->whereIn("schdl_title",$templateTitles)
-            ->update(["schdl_is_template"=>"1"]);
+            ->map(function ($tt) {
+                return $tt->title;
+            })->toArray();
+        DB::table("schedules")->update(["schdl_is_template" => "0"]);
+        DB::table("schedules")->whereIn("schdl_title", $templateTitles)
+            ->update(["schdl_is_template" => "1"]);
 
         Schema::drop('section_template');
         Schema::drop('shift_template');
