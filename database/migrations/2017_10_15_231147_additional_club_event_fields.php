f02a6c32430576073b7361f27e1c54250cb00b84
fabian <f.markert87@googlemail.com>
2018-01-29 22:19:47 +0100

    don't show additional fields if not set

--- database/migrations/2017_10_15_231147_additional_club_event_fields.php
+++ database/migrations/2017_10_15_231147_additional_club_event_fields.php
@@ -14,12 +14,12 @@ class AdditionalClubEventFields extends Migration
     public function up()
     {
         Schema::table('club_events', function (Blueprint $table) {
-            $table->boolean("facebook_done")->default(false);
-            $table->mediumText("event_url")->default("");
-            $table->float("price_tickets_normal");
-            $table->float("price_tickets_external");
-            $table->float("price_normal");
-            $table->float("price_external");
+            $table->boolean("facebook_done")->nullable();
+            $table->mediumText("event_url")->nullable();
+            $table->float("price_tickets_normal")->nullable();
+            $table->float("price_tickets_external")->nullable();
+            $table->float("price_normal")->nullable();
+            $table->float("price_external")->nullable();
         });
     }
 
@@ -33,7 +33,7 @@ class AdditionalClubEventFields extends Migration
         Schema::table('club_events', function (Blueprint $table) {
             $table->dropColumn('facebook_done');
             $table->dropColumn('event_url');
-    
+
             $table->dropColumn('price_tickets_normal');
             $table->dropColumn('price_tickets_external');
             $table->dropColumn('price_normal');
