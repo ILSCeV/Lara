7db989e89efc2dbae06effc95432bab79cb5e317
Stefan Heyder <stefan.heyder@gmail.com>
2018-02-12 17:25:02 +0100

    Fix imports in views

--- resources/views/partials/statistics/leaderboardsOfClub.blade.php
+++ resources/views/partials/statistics/leaderboardsOfClub.blade.php
@@ -1,4 +1,4 @@
-<div class="tab-pane fade in {{ Section::sectionOfCurrentUser() === $name? 'active': '' }}" id="{{$name}}Leaderboards">
+<div class="tab-pane fade in {{ Lara\Section::sectionOfCurrentUser() === $name? 'active': '' }}" id="{{$name}}Leaderboards">
     <table class="table table-hover">
         <thead>
         <tr>
