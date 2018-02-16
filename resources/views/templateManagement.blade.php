c2907d229b2c7f0951f9a437c4b0a992a042db11
4D44H <4d44hex@gmail.com>
2018-02-16 21:44:21 +0100

    Minor fix

--- resources/views/templateManagement.blade.php
+++ resources/views/templateManagement.blade.php
@@ -13,81 +13,81 @@
 
         <div class="panel panel-body no-padding">
             <table class="table info table-hover table-condensed">
-                    <thead>
-                        <tr class="active">
-                            <th class="col-md-1 col-xs-1 padding-left-15">
-                                #
-                            </th>
-                            <th class="col-md-1 col-xs-1">
-                                {{ trans('mainLang.section') }}
-                            </th>
-                            <th class="col-md-3 col-xs-3">
-                                {{ trans('mainLang.type') }}
-                            </th>
-                            <th class="col-md-5 col-xs-5">
-                                {{ trans('mainLang.title') }}
-                            </th>
-                            <th class="col-md-1 col-xs-1">
-                                {{ trans('mainLang.start') }}
-                            </th>
-                            <th class="col-md-1 col-xs-1">
-                                {{ trans('mainLang.end') }}
-                            </th>
-                            <th class="col-md-1 col-xs-1 padding-right-15">
-                                &nbsp;
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="container" id="templateOverviewTable">
-                        <div class="table-control">
-                            <span class="table-control__create-new-template">
-                                <a class="btn btn-success" href="{{route('template.create')}}">
-                                    {{ trans('mainLang.createTemplate') }}
-                                </a>
-                            </span>
-                            <span class="table-control__search fa-pull-right form-inline has-feedback">
-                               <label for="templateOverviewFilter" class="test"> {{ trans('mainLang.search') }}: </label>
-                               <input type="text" class="form-control" id="templateOverviewFilter">
-                            </span>
-                        </div>
+                <thead>
+                    <tr class="active">
+                        <th class="col-md-1 col-xs-1 padding-left-15">
+                            #
+                        </th>
+                        <th class="col-md-1 col-xs-1">
+                            {{ trans('mainLang.section') }}
+                        </th>
+                        <th class="col-md-3 col-xs-3">
+                            {{ trans('mainLang.type') }}
+                        </th>
+                        <th class="col-md-5 col-xs-5">
+                            {{ trans('mainLang.title') }}
+                        </th>
+                        <th class="col-md-1 col-xs-1">
+                            {{ trans('mainLang.start') }}
+                        </th>
+                        <th class="col-md-1 col-xs-1">
+                            {{ trans('mainLang.end') }}
+                        </th>
+                        <th class="col-md-1 col-xs-1 padding-right-15">
+                            &nbsp;
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="container" id="templateOverviewTable">
+                    <div class="table-control">
+                        <span class="table-control__create-new-template">
+                            <a class="btn btn-success" href="{{route('template.create')}}">
+                                {{ trans('mainLang.createTemplate') }}
+                            </a>
+                        </span>
+                        <span class="table-control__search fa-pull-right form-inline has-feedback">
+                           <label for="templateOverviewFilter" class="test"> {{ trans('mainLang.search') }}: </label>
+                           <input type="text" class="form-control" id="templateOverviewFilter">
+                        </span>
+                    </div>
 
-                        @foreach($templates as $template)
-                            <tr>
-                                <td class="padding-left-15">
-                                    {!! $template->id !!}
-                                </td>
-                                <td>
-                                    {{ $template->section->title }}
-                                </td>
-                                <td>
-                                    {{ \Lara\Utilities::getEventTypeTranslation($template->type)  }}
-                                </td>
-                                <td>
-                                    <a href="{{ route('template.edit', $template) }}"> {{ $template->title }} </a>
-                                </td>
-                                <td>
-                                    {{ $template->time_start }}
-                                </td>
-                                <td>
-                                    {{ $template->time_end }}
-                                </td>
-                                <td class="padding-right-15"> 
-                                    <button data-id="{{$template->id}}" 
-                                            data-templatename="{{$template->title}}" 
-                                            class="btn btn-danger delete-template"> 
-                                        <span class="glyphicon glyphicon-trash"></span> 
-                                    </button>
-                                    <form id="delete-template-{{$template->id}}" 
-                                          method="POST" 
-                                          class="hidden" 
-                                          action="{{route('template.delete', $template->id)}}">
-                                        {{ csrf_field() }}
-                                        <button class="hidden" type="submit"></button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
+                    @foreach($templates as $template)
+                        <tr>
+                            <td class="padding-left-15">
+                                {!! $template->id !!}
+                            </td>
+                            <td>
+                                {{ $template->section->title }}
+                            </td>
+                            <td>
+                                {{ \Lara\Utilities::getEventTypeTranslation($template->type)  }}
+                            </td>
+                            <td>
+                                <a href="{{ route('template.edit', $template) }}"> {{ $template->title }} </a>
+                            </td>
+                            <td>
+                                {{ $template->time_start }}
+                            </td>
+                            <td>
+                                {{ $template->time_end }}
+                            </td>
+                            <td class="padding-right-15"> 
+                                <button data-id="{{$template->id}}" 
+                                        data-templatename="{{$template->title}}" 
+                                        class="btn btn-danger delete-template"> 
+                                    <span class="glyphicon glyphicon-trash"></span> 
+                                </button>
+                                <form id="delete-template-{{$template->id}}" 
+                                      method="POST" 
+                                      class="hidden" 
+                                      action="{{route('template.delete', $template->id)}}">
+                                    {{ csrf_field() }}
+                                    <button class="hidden" type="submit"></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
             </table>
         </div>
     </div>
