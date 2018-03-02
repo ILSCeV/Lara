f462bc78863a5af6c44bd0480e2766a255f28559
fabian <f.markert87@googlemail.com>
2018-03-02 23:50:12 +0100

    ignore shifts without an event

--- resources/views/shiftTypeView.blade.php
+++ resources/views/shiftTypeView.blade.php
@@ -19,16 +19,16 @@
 		<div class="panel panel-body no-padding">
 			<table class="table table-hover">
 				{!! Form::open(  array( 'route' => ['shiftType.update', $current_shiftType->id],
-		                                'id' => $current_shiftType->id, 
-		                                'method' => 'PUT', 
+		                                'id' => $current_shiftType->id,
+		                                'method' => 'PUT',
 		                                'class' => 'shiftType')  ) !!}
 					<tr>
 						<td width="20%" class="left-padding-16">
 							<i>{{ trans('mainLang.shiftType') }}:</i>
 						</td>
 						<td>
-							{!! Form::text('title' . $current_shiftType->id, 
-							   $current_shiftType->title, 
+							{!! Form::text('title' . $current_shiftType->id,
+							   $current_shiftType->title,
 							   array('id'=>'title' . $current_shiftType->id)) !!}
 						</td>
 					</tr>
@@ -37,8 +37,8 @@
 							<i>{{ trans('mainLang.begin') }}:</i>
 						</td>
 						<td>
-							{!! Form::input('time','start' . $current_shiftType->id, 
-							   $current_shiftType->start, 
+							{!! Form::input('time','start' . $current_shiftType->id,
+							   $current_shiftType->start,
 							   array('id'=>'start' . $current_shiftType->id)) !!}
 						</td>
 					</tr>
@@ -47,8 +47,8 @@
 							<i>{{ trans('mainLang.end') }}:</i>
 						</td>
 						<td>
-							{!! Form::input('time','end' . $current_shiftType->id, 
-							   $current_shiftType->end, 
+							{!! Form::input('time','end' . $current_shiftType->id,
+							   $current_shiftType->end,
 							   array('id'=>'end' . $current_shiftType->id)) !!}
 						</td>
 					</tr>
@@ -57,8 +57,8 @@
 							<i>{{ trans('mainLang.weight') }}:</i>
 						</td>
 						<td>
-							{!! Form::text('statistical_weight' . $current_shiftType->id, 
-							   $current_shiftType->statistical_weight, 
+							{!! Form::text('statistical_weight' . $current_shiftType->id,
+							   $current_shiftType->statistical_weight,
 							   array('id'=>'statistical_weight' . $current_shiftType->id)) !!} <br/>
 						</td>
 					</tr>
@@ -121,11 +121,16 @@
 								</thead>
 								<tbody>
 									@foreach($shifts as $shift)
+                                        {{-- ignore shifts without an event, example: shifts from bd-template  --}}
+                                        @if(is_null($shift->schedule->evnt_id))
+                                            @continue
+                                        @endif
+
 										<tr class="{!! "shiftType-event-row" . $shift->id !!}" name="{!! "shiftType-event-row" . $shift->id !!}">
 											<td>
 										      	{!! $shift->schedule->event->id !!}
 											</td>
-											<td>						
+											<td>
 												<a href="/event/{!! $shift->schedule->event->id !!}">{!! $shift->schedule->event->evnt_title !!}</a>
 											</td>
 											<td>
@@ -147,11 +152,11 @@
 
 									           		<div class="btn-group dropdown-shiftTypes">
 
-													  	<a href="#" 
-													  	   class="btn btn-small btn-default" 
+													  	<a href="#"
+													  	   class="btn btn-small btn-default"
 													  	   name={{ "dropdown" . $shift->id }}
 										           		   id={{   "dropdown" . $shift->id }}
-										           		   data-toggle="dropdown" 
+										           		   data-toggle="dropdown"
 										           		   aria-expanded="true">
 										           		   		{{ trans('mainLang.substituteThisInstance') }}
 													  			<span class="caret"></span>
@@ -160,13 +165,13 @@
 														<ul class="dropdown-menu">
 															@foreach($shiftTypes as $shiftType)
 																@if($shiftType->id !== $current_shiftType->id)
-																	<li class="dropdown"> 
-																		<a href="javascript:void(0);" 
+																	<li class="dropdown">
+																		<a href="javascript:void(0);"
 																		   onClick="document.getElementById('{{ 'shift'. $shift->id }}').value='{{ $shift->id }}';
 																					document.getElementById('{{ 'shiftType'. $shift->id }}').value='{{ $shiftType->id }}';
 																					document.getElementById('{{ 'btn-submit-changes'. $shift->id }}').click();">
-																		   	(#{{ $shiftType->id }}) 
-																		   	{{  $shiftType->title }} 
+																		   	(#{{ $shiftType->id }})
+																		   	{{  $shiftType->title }}
 																		   	(<i class='fa fa-clock-o'></i>
 																			{{  date("H:i", strtotime($shiftType->start))
 																				. "-" .
@@ -192,11 +197,11 @@
 			</table>
 		</div>
 	</div>
-		
+
 	<center>
 		{{ $shifts->links() }}
 	</center>
-	
+
 	<br/>
 @else
 	@include('partials.accessDenied')
