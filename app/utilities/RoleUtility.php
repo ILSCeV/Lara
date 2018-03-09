e00262a26c99012e645a9901e725930b5ac8d765
fabian <f.markert87@googlemail.com>
2018-03-09 02:02:07 +0100

    add role utility

---
+++ app/utilities/RoleUtility.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * User: fabian
+ * Date: 09.03.18
+ * Time: 01:38
+ */
+
+namespace Lara\utilities;
+
+
+use Illuminate\Contracts\Logging\Log;
+use Lara\Role;
+use Lara\Section;
+use Lara\User;
+
+class RoleUtility
+{
+    const PRIVILEGE_ADMINISTRATOR = "admin";
+
+    const PRIVILEGE_CL = "clubleitung";
+
+    const PRIVILEGE_MARKETING = "marketing";
+
+    const PRIVILEGE_MEMBER = "mitglied";
+
+    const ALL_PRIVILEGES = [self::PRIVILEGE_CL, self::PRIVILEGE_ADMINISTRATOR, self::PRIVILEGE_MARKETING, self::PRIVILEGE_MEMBER];
+
+    public static function assignPrivileges(User $user, Section $section, $privilege)
+    {
+        switch ($privilege) {
+            case self::PRIVILEGE_ADMINISTRATOR:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_ADMINISTRATOR)->first()->users()->attach($user);
+            case self::PRIVILEGE_CL:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_CL)->first()->users()->attach($user);
+            case self::PRIVILEGE_MARKETING:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_MARKETING)->first()->users()->attach($user);
+            case self::PRIVILEGE_MEMBER:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_MEMBER)->first()->users()->attach($user);
+                break;
+            default:
+                Log::warn('could not assign privilege ' . $privilege, [$user, $section]);
+        }
+    }
+
+    public static function removePrivileges(User $user, Section $section, $privilege)
+    {
+        switch ($privilege) {
+            case self::PRIVILEGE_ADMINISTRATOR:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_ADMINISTRATOR)->first()->users()->detach($user);
+            case self::PRIVILEGE_CL:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_CL)->first()->users()->detach($user);
+            case self::PRIVILEGE_MARKETING:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_MARKETING)->first()->users()->detach($user);
+            case self::PRIVILEGE_MEMBER:
+                Role::where('section_id', '=', $section->id)->where('name', '=', self::PRIVILEGE_MEMBER)->first()->users()->detach($user);
+                break;
+            default:
+                Log::warn('could not assign privilege ' . $privilege, [$user, $section]);
+        }
+    }
+}
