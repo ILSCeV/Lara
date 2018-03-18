2aad5aee60bea1eace9a4b31e0a4d6a78169d54d
Stefan Heyder <stefan.heyder@gmail.com>
2018-03-18 11:02:06 +0100

    Update old permission system for shift management

--- resources/views/shifttypes/shiftTypeView.blade.php
+++ resources/views/shifttypes/shiftTypeView.blade.php
@@ -7,11 +7,7 @@
 
 @section('content')
 
-@if(Session::has('userGroup')
-	AND (Session::get('userGroup') == 'marketing'
-	OR Session::get('userGroup') == 'clubleitung'
-	OR Session::get('userGroup') == 'admin'))
-
+@is(['admin', 'clubleitung', 'marketing'])
 	<div class="panel panel-info">
 		<div class="panel-heading">
 			<h4 class="panel-title">#{{ $current_shiftType->id }}: "{!! $current_shiftType->title !!}" </h4>
@@ -232,7 +228,7 @@
 	<br/>
 @else
 	@include('partials.accessDenied')
-@endif
+@endis
 @stop
 
 
