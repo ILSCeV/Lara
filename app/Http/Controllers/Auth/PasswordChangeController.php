f773416beeee4e30ffa6c99b4e21274631735689
Fabian Markert <f.markert87@googlemail.com>
2018-04-04 13:31:19 +0200

    use ldap utility for common operations

--- app/Http/Controllers/Auth/PasswordChangeController.php
+++ app/Http/Controllers/Auth/PasswordChangeController.php
@@ -2,13 +2,13 @@
 
 namespace Lara\Http\Controllers\Auth;
 
-use Illuminate\Support\Facades\Config;
-use Lara\Http\Controllers\Controller;
-use Lara\Utilities;
-
 use Auth;
-use Input;
 use Hash;
+use Input;
+use Lara\Http\Controllers\Controller;
+use Lara\User;
+use Lara\Utilities;
+use Lara\utilities\LdapUtility;
 use Redirect;
 use Validator;
 use View;
@@ -24,7 +24,7 @@ class PasswordChangeController extends Controller
     {
         // Get old password and check that it matches the one stored in the DB
         $validator = $this->validator(Input::all());
-        if($validator->fails()){
+        if ($validator->fails()) {
             return Redirect::back()->withErrors($validator)->withInput();
         }
         $oldPassword = Input::get('old-password');
@@ -42,10 +42,10 @@ class PasswordChangeController extends Controller
         $user->password = bcrypt($newPassword);
         $user->save();
 
-        if (!\App::environment('development') && strpos(env('LDAP_SECTIONS',''), $user->section->title ) !== false) {
+        if (!\App::environment('development') && strpos(env('LDAP_SECTIONS', ''), $user->section->title) !== false) {
             try {
                 $this->changePasswordInLDAP($user);
-            } catch (\Exception $e){
+            } catch (\Exception $e) {
                 \Log::error("ldap broken", $e);
             }
         }
@@ -66,58 +66,9 @@ class PasswordChangeController extends Controller
     /**
      * @param $user
      */
-    private function changePasswordInLDAP($user)
+    private function changePasswordInLDAP(User $user)
     {
-// CONNECTING TO LDAP SERVER
-        $ldapConn = ldap_connect(Config::get('bcLDAP.server'), Config::get('bcLDAP.port'));
-
-        // Set some ldap options for talking to AD
-        // LDAP_OPT_PROTOCOL_VERSION: LDAP protocol version
-        ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        // LDAP_OPT_REFERRALS: Specifies whether to automatically follow referrals returned by the LDAP server
-        ldap_set_option($ldapConn, LDAP_OPT_REFERRALS, 0);
-
-        // Bind as a domain admin
-        $ldap_bind = ldap_bind($ldapConn,
-            Config::get('bcLDAP.admin-username'),
-            Config::get('bcLDAP.admin-password'));
-
-        // Search for a bc-Club user with the uid number entered
-        $search = ldap_search($ldapConn,
-            Config::get('bcLDAP.bc-club-ou') .
-            Config::get('bcLDAP.base-dn'),
-            '(uid=' . $user->person->prsn_ldap_id . ')');
-
-        $info = ldap_get_entries($ldapConn, $search);
-        $userFound = false;
-        // Set default user access group to "bc-Club member"
-        if ($info['count'] != 0) {
-            $userFound = true;
-        } else {
-            $search = ldap_search($ldapConn,
-                Config::get('bcLDAP.bc-cafe-ou') .
-                Config::get('bcLDAP.base-dn'),
-                '(uid=' . $user->person->prsn_ldap_id . ')');
-            $info = ldap_get_entries($ldapConn, $search);
-            if ($info['count'] != 0) {
-                $userFound = true;
-            }
-        }
-
-        if ($userFound) {
-            $userEntry = ldap_first_entry($ldapConn, $search);
-            $userDn = ldap_get_dn($ldapConn, $userEntry);
-            // Hashing password input
-            $encoded_newPassword = '{md5}' . base64_encode(mhash(MHASH_MD5, Input::get('password')));
-
-            $entry = array();
-            $entry["userPassword"] = "$encoded_newPassword";
-
-            if (!ldap_modify($ldapConn, $userDn, $entry)) {
-                \Log::error("ldap change not worked");
-            }
-        }
-
-        ldap_unbind($ldapConn);
+        $encoded_newPassword = '{md5}' . base64_encode(mhash(MHASH_MD5, Input::get('password')));
+        LdapUtility::changePassword($user->person->prsn_ldap_id, $encoded_newPassword);
     }
 }
