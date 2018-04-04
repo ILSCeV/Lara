80bc4de2f801f2e487dbf24d20701877b30be06f
Fabian Markert <f.markert87@googlemail.com>
2018-04-04 23:33:17 +0200

    fix typo

--- database/migrations/2018_04_04_134812_create_ldap_platforms_table.php
+++ database/migrations/2018_04_04_134812_create_ldap_platforms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLdapPlattformsTable extends Migration
+class CreateLdapPlatformsTable extends Migration
 {
     /**
      * Run the migrations.
