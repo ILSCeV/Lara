ddf69c3020bec136ba5eb97e6a465b9b82a8b132
bedo2991 <fra.bdn@gmail.com>
2018-04-24 23:45:00 +0200

    Add survey option

--- resources/views/partials/filter.blade.php
+++ resources/views/partials/filter.blade.php
@@ -3,10 +3,12 @@
 	@foreach($sections as $section)
 		<span id="label-{!! $section["title"] !!}" class="label label-filters palette-{{$section->color}}-500-Primary bg">{!! $section["title"] !!}</span>
 	@endforeach
+	<span id="label-survey" class="label label-filters palette-Purple-900-Primary bg">{{trans('mainLang.survey')}}</span>
 	<select id="section-filter-selector" class="hidden show-tick" multiple title="{{ trans('mainLang.chooseAtLeastOne') }}" data-selected-text-format="count > 2" data-actions-box="true">
 	@foreach($sections as $section)
 		<option value="filter-{!! $section["title"] !!}" class="palette-{{$section->color}}-500-Primary bg option-shadow">{!! $section["title"] !!}</option>
 	@endforeach
+		<option value="filter-survey" class="palette-Purple-900-Primary bg option-shadow">{{trans('mainLang.survey')}}</option>
 	</select>
 </div>
 
