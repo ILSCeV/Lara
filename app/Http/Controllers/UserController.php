2d5958a2aa2dd204a4319c746facf70dcf253896
bedo2991 <fra.bdn@gmail.com>
2018-05-20 22:48:12 +0200

    Allow not CL user to change their privacy flag

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-       $this->middleware(ClOnly::class);
+       $this->middleware(ClOnly::class, ['except' => ['agreePrivacy']]);
     }
 
     /**
