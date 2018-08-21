07f4b5acdbcbf30e38d114a0b7938890fe5f9b99
Stefan Heyder <stefan.heyder@gmail.com>
2018-08-21 21:25:41 +0200

    Fix display for large amount of sections
    Using the class "pull-back" will reorder the pulled items, so we changed
    the order of arrangement.

--- resources/views/partials/filter.blade.php
+++ resources/views/partials/filter.blade.php
@@ -1,35 +1,33 @@
 <div id="section-filter" class="hidden-print">
-	{{-- Show/hide events belonging to a chosen section --}}
-	@foreach($sections as $section)
-		<span id="label-{!! $section["title"] !!}" class="label label-filters palette-{{$section->color}}-500-Primary bg hidden">
-            {!! $section["title"] !!}
+    {{-- Show/hide events belonging to a chosen section --}}
+    <div class="col-md-12">
+        <span id="label-none" class="label label-filters palette-Red-900-Primary bg hidden pull-right" >&nbsp;{{trans('mainLang.noSectionSelected')}}
+        </span>
+        <span id="label-survey" class="label label-filters palette-Purple-900 bg hidden pull-right" >{{trans('mainLang.survey')}}
             &nbsp;
             <span class="glyphicon glyphicon-remove-circle"></span>
         </span>
-	@endforeach
-	<span id="label-survey" class="label label-filters palette-Purple-900 bg hidden" >{{trans('mainLang.survey')}}
-        &nbsp;
-        <span class="glyphicon glyphicon-remove-circle"></span>
-     </span>
-    <span id="label-none" class="label label-filters palette-Red-900-Primary bg hidden" >&nbsp;{{trans('mainLang.noSectionSelected')}}
-     </span>
-    <br class="visible-xs">
-    <br class="visible-xs">
-    <span class="hidden-xs">
-        &nbsp;
-        &nbsp;
-    </span>
-	<select id="section-filter-selector" class="hidden show-tick" multiple
-            title="{{ trans('mainLang.chooseAtLeastOne') }}"
-            data-selected-text-format="count > 2"
-            data-actions-box="true"
-            data-select-all-text="{{ trans('mainLang.selectAll') }}"
-            data-deselect-all-text="{{ trans('mainLang.selectNone') }}"
-            data-count-selected-text="{{ trans('mainLang.countSectionsSelected') }}">
-	@foreach($sections as $section)
-		<option value="filter-{!! $section["title"] !!}" class="palette-{{$section->color}}-500-Primary bg option-shadow">{!! $section["title"] !!}</option>
-	@endforeach
-		<option value="filter-survey" class="palette-Purple-900 bg option-shadow">{{trans('mainLang.survey')}}</option>
-	</select>
+        @foreach($sections->reverse() as $section)
+            <span id="label-{!! $section["title"] !!}" class="label label-filters palette-{{$section->color}}-500-Primary bg hidden pull-right">
+                {!! $section["title"] !!}
+                &nbsp;
+                <span class="glyphicon glyphicon-remove-circle"></span>
+            </span>
+        @endforeach
+    </div>
+    <div class="col-md-12">
+        <select id="section-filter-selector" class="hidden show-tick pull-right" multiple
+                                             title="{{ trans('mainLang.chooseAtLeastOne') }}"
+                                             data-selected-text-format="count > 2"
+                                             data-actions-box="true"
+                                             data-select-all-text="{{ trans('mainLang.selectAll') }}"
+                                             data-deselect-all-text="{{ trans('mainLang.selectNone') }}"
+                                             data-count-selected-text="{{ trans('mainLang.countSectionsSelected') }}">
+            @foreach($sections as $section)
+                <option value="filter-{!! $section["title"] !!}" class="palette-{{$section->color}}-500-Primary bg option-shadow">{!! $section["title"] !!}</option>
+            @endforeach
+            <option value="filter-survey" class="palette-Purple-900 bg option-shadow">{{trans('mainLang.survey')}}</option>
+        </select>
+    </div>
 </div>
 
