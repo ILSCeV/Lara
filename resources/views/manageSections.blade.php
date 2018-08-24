d5a67a973900b6230846d03e7e79bb90b7a8b2e2
fabian <f.markert87@googlemail.com>
2018-08-24 18:11:10 +0200

    add ability to manage sections for CL, create and delete is admin only

--- resources/views/manageSections.blade.php
+++ resources/views/manageSections.blade.php
@@ -3,68 +3,64 @@
 @extends('layouts.master')
 
 @section('title')
-	{{ trans('mainLang.manageSections') }} 
+    {{ trans('mainLang.manageSections') }}
 @stop
 
 @section('content')
 
-@is('admin')
-
-	<div class="panel panel-info col-xs-12 no-padding">
-		<div class="panel-heading">
-				<h4 class="panel-title">{{ trans('mainLang.management') }}: {{ trans('mainLang.manageSections') }}</h4>
-		</div>	
-		<div class="panel panel-body no-padding">	
-			<table class="table info table-hover table-condensed">
-				<thead>
-					<tr class="active">
-						<th class="col-md-1 col-xs-1">
-							#
-						</th>
-						<th class="col-md-2 col-xs-2">
-							{{ trans('mainLang.section') }}
-						</th>
-						<th class="col-md-4 col-xs-4">
-							{{ trans("mainLang.color") }} 
-						</th>
-					</tr>
-				</thead>
-				<tbody class="container">
-						@foreach($sections as $section)
-							<tr>
-								<td>
-									{!! $section->id !!}
-								</td>
-								<td>
-							      	<a href="../section/{{ $section->id }}">
-							      		{!! $section->title !!}
-							      	</a>
-								</td>
-								<td>
+    <div class="panel panel-info col-xs-12 no-padding">
+        <div class="panel-heading">
+            <h4 class="panel-title">{{ trans('mainLang.management') }}: {{ trans('mainLang.manageSections') }}</h4>
+        </div>
+        <div class="panel panel-body no-padding">
+            <table class="table info table-hover table-condensed">
+                <thead>
+                <tr class="active">
+                    <th class="col-md-1 col-xs-1">
+                        #
+                    </th>
+                    <th class="col-md-2 col-xs-2">
+                        {{ trans('mainLang.section') }}
+                    </th>
+                    <th class="col-md-4 col-xs-4">
+                        {{ trans("mainLang.color") }}
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="container">
+                @foreach($sections as $section)
+                    <tr>
+                        <td>
+                            {!! $section->id !!}
+                        </td>
+                        <td>
+                            <a href="../section/{{ $section->id }}">
+                                {!! $section->title !!}
+                            </a>
+                        </td>
+                        <td>
 									<span class="palette-{{$section->color}}-500-Primary bg">
 										&nbsp;&nbsp;&nbsp;&nbsp;{!! $section->color !!}&nbsp;&nbsp;&nbsp;&nbsp;
 									</span>
-								</td>
-							</tr>
-						@endforeach
-				<tr>
-					<td></td>
-					<td> <a class="btn btn-success" href="{!! action('SectionController@create') !!}">{{ trans('mainLang.createSection') }}</a> </td>
-					<td></td>
-				</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+                        </td>
+                    </tr>
+                @endforeach
+                @is('admin')
+                <tr>
+                    <td></td>
+                    <td><a class="btn btn-success"
+                           href="{!! action('SectionController@create') !!}">{{ trans('mainLang.createSection') }}</a>
+                    </td>
+                    <td></td>
+                </tr>
+                @endis
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-		<div class="text-center">
-		{{ $sections->links() }}
-		</div>
+    <br/>
 
-	<br/>
-@else
-	@include('partials.accessDenied')
-@endis
 @stop
 
 
