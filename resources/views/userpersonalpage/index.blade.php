0b228bec5ee9730ff277ddb3012fd99330211492
4D44H <4d44hex@gmail.com>
2018-09-04 21:51:30 +0200

    Redesign personal page
    - make event titles clickable
    - correct padding and margins
    - add shift end time
    - reformat privacy setting as divs instead of a table (scaffolds better on mobile)

--- resources/views/userpersonalpage/index.blade.php
+++ resources/views/userpersonalpage/index.blade.php
@@ -6,95 +6,91 @@
     */
 @endphp
 @section('title')
-    {{ trans('mainLang.userPersonalPage') }} - {{ $user->name }}
+    {{ trans('mainLang.userPersonalPage') }} - {{ $user->name }} ({{ $user->section->title }})
 @endsection
 
 @section('content')
     <div class="container-fluid">
         <div class="panel panel-info">
             <div class="panel-heading">
-                <h4 class="panel-title">{{ trans('mainLang.userPersonalPage') }}: {{ $user->name }}
-                    - {{ $user->section->title }}</h4>
+                <h4 class="panel-title">
+                    {{ trans('mainLang.userPersonalPage') }}:
+                    {{ $user->name }} ({{ $user->section->title }})
+                </h4>
             </div>
-            <div class="panel-body">
-                <div class="col-md-12 ">
+            <div class="panel-body no-padding">
+                <div class="all-sides-padding-16">
                     {{Form::open(['route' => 'user.updatePersonalSettings'])}}
-                    <table class="table table-hover">
-                        {{-- is club name public --}}
-                        <tr class="form-group">
-                            <td width="20%" class="left-padding-16">
-                                <label for="endTime">
-                                    <i>{{ trans('mainLang.privateClubName') }}</i>
-                                </label>
-                            </td>
-                            <td>
-							<span class="col-md-12 col-sm-12 col-xs-12 no-padding">
-                                <div class="radio-inline">
-                                    <label>
-                                        {!! Form::radio( 'is_name_private','null', $user->is_name_private === null) !!}
-                                        {{ trans('mainLang.privateClubNameNull') }}
-                                    </label>
-                                </div>
-                                <div class="radio-inline">
-                                    <label>
-                                        {!! Form::radio( 'is_name_private','true', $user->is_name_private === 1) !!}
-                                        {{ trans('mainLang.privateClubNameYes') }}
-                                    </label>
-                                </div>
-                                <div class="radio-inline">
-                                    <label>
-                                        {!! Form::radio( 'is_name_private','false', $user->is_name_private === 0) !!}
-                                        {{ trans('mainLang.privateClubNameNo') }}
-                                    </label>
-                                </div>
-                            </span>
-                            </td>
-                        </tr>
-                    </table>
-                    <div>
+                        <label for="privateClubName">
+                            <strong>{{ trans('mainLang.privateClubName') }}</strong>
+                        </label>
+                        <br class="show-xs">
+                        <div class="radio-inline">
+                            <label>
+                                {!! Form::radio( 'is_name_private','null', $user->is_name_private === null) !!}
+                                {{ trans('mainLang.privateClubNameNull') }}
+                            </label>
+                        </div>
+                        <br>
+                        <div class="radio-inline">
+                            <label>
+                                {!! Form::radio( 'is_name_private','true', $user->is_name_private === 1) !!}
+                                {{ trans('mainLang.privateClubNameYes') }}
+                            </label>
+                        </div>
+                        <br>
+                        <div class="radio-inline">
+                            <label>
+                                {!! Form::radio( 'is_name_private','false', $user->is_name_private === 0) !!}
+                                {{ trans('mainLang.privateClubNameNo') }}
+                            </label>
+                        </div>
+                        <br>
+                        <br>
                         <div class="btn-group">
                             <button type="reset"
-                                    class="btn btn-small btn-default">{{ trans('mainLang.reset') }}</button>
+                                    class="btn btn-default">{{ trans('mainLang.reset') }}</button>
                             <button type="submit"
-                                    class="btn btn-small btn-success">{{ trans('mainLang.update') }}</button>
+                                    class="btn btn-success">{{ trans('mainLang.update') }}</button>
                         </div>
-                    </div>
                     {{Form::close()}}
                 </div>
-                <div>
-                    <table class="col-md-12">
-                        <caption class="text-center">{{trans('mainLang.upcomingShifts')}}:</caption>
-                        <thead>
+
+                <br>
+
+                <h5 class="left-padding-16 no-margin">
+                    {{trans('mainLang.upcomingShifts')}}:
+                </h5>
+
+                <table class="table table-hover col-md-12">
+                    <caption class="text-center"></caption>
+                    <thead>
                         <tr>
                             <th class="text-center col-md-3">{{trans('mainLang.shift')}}</th>
                             <th class="text-center col-md-3">{{trans('mainLang.event')}}</th>
                             <th class="text-center col-md-3">{{trans('mainLang.date')}}</th>
-                            <th class="text-center col-md-3">{{trans('mainLang.begin')}}</th>
+                            <th class="text-center col-md-3">{{trans('mainLang.begin')}}-{{trans('mainLang.end')}}</th>
                         </tr>
-                        </thead>
-                        <tbody>
-                        @php
-                            /** @var \Lara\Shift $shift */
-                        @endphp
+                    </thead>
+                    <tbody>
                         @foreach($shifts as $shift )
                             <tr>
                                 <td class="text-center">
                                     {{ $shift->type->title }}
                                 </td>
                                 <td class="text-center">
-                                    {{ $shift->schedule->event->evnt_title }}
+                                    <a href="/event/{{$shift->schedule->event->id}}">{{ $shift->schedule->event->evnt_title }}</a>
                                 </td>
                                 <td class="text-center">
-                                    {{ (new DateTime($shift->schedule->event->evnt_date_start))->format('d.m.Y') }}
+                                    {{ (new DateTime($shift->schedule->event->evnt_date_start))->format('D, d.m.Y') }}
                                 </td>
                                 <td class="text-center">
-                                    {{ (new DateTime($shift->start))->format('H:i') }}
+                                    {{ (new DateTime($shift->start))->format('H:i') }}-{{ (new DateTime($shift->end))->format('H:i') }}
                                 </td>
                             </tr>
                         @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
