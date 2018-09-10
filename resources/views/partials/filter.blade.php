135782e617d1bec57a4f644b0a1a9127678a9800
4D44H <4d44hex@gmail.com>
2018-09-10 22:48:40 +0200

    Add padding in section filter (month & week views)

--- resources/views/partials/filter.blade.php
+++ resources/views/partials/filter.blade.php
@@ -1,6 +1,6 @@
 <div id="section-filter" class="hidden-print">
     {{-- Show/hide events belonging to a chosen section --}}
-    <div class="col-md-12">
+    <div class="col-md-12 no-padding">
         <span id="label-none" class="label label-filters palette-Red-900-Primary bg hidden pull-right" >&nbsp;{{trans('mainLang.noSectionSelected')}}
         </span>
         <span id="label-survey" class="label label-filters palette-Purple-900 bg hidden pull-right" >{{trans('mainLang.survey')}}
@@ -15,8 +15,10 @@
             </span>
         @endforeach
     </div>
-    <div class="col-md-12">
-        <select id="section-filter-selector" class="hidden show-tick pull-right" multiple
+    <br>
+    <div class="col-md-12 no-padding">
+        <select id="section-filter-selector" class="hidden show-tick pull-right bottom-padding"
+                                             multiple
                                              title="{{ trans('mainLang.chooseAtLeastOne') }}"
                                              data-selected-text-format="count > 2"
                                              data-actions-box="true"
@@ -24,10 +26,15 @@
                                              data-deselect-all-text="{{ trans('mainLang.selectNone') }}"
                                              data-count-selected-text="{{ trans('mainLang.countSectionsSelected') }}">
             @foreach($sections as $section)
-                <option value="filter-{!! $section["title"] !!}" class="palette-{{$section->color}}-500-Primary bg option-shadow">{!! $section["title"] !!}</option>
+                <option value="filter-{!! $section["title"] !!}"
+                        class="palette-{{$section->color}}-500-Primary bg option-shadow">
+                            {!! $section["title"] !!}
+                </option>
             @endforeach
-            <option value="filter-survey" class="palette-Purple-900 bg option-shadow">{{trans('mainLang.survey')}}</option>
+            <option value="filter-survey"
+                    class="palette-Purple-900 bg option-shadow">
+                        {{trans('mainLang.survey')}}
+            </option>
         </select>
     </div>
 </div>
-
