ee2c190f61baaec959ed712f532e6756d5c22bdf
4D44H <4d44hex@gmail.com>
2018-09-17 16:45:16 +0200

    Add color background and icons to day view

--- resources/views/partials/clubEventByIdSmall.blade.php
+++ resources/views/partials/clubEventByIdSmall.blade.php
@@ -1,21 +1,45 @@
 
 <div class="panel">
-	<div class="panel panel-heading">
-		{{-- show only a placeholder for private events --}}
-		@if($clubEvent->evnt_is_private && !Auth::user())
-			<h4><span class="name">{{ trans('mainLang.internalEventP') }}</span></h4>
-		@else
-			<h4><a href="{{ URL::route('event.show', $clubEvent->id) }}">{{ $clubEvent->evnt_title }}</a></h4>
-		@endif
-	</div>
+    {{-- show only a placeholder for private events --}}
+    @if($clubEvent->evnt_is_private && !Auth::user())
+        <div class="panel panel-heading">
+            <h4><span class="name">{{ trans('mainLang.internalEvent') }}</span></h4>
+        </div>
+    @else
+        {{-- Set panel color --}}
+        @if     ($clubEvent->evnt_type == 0)
+            <div class="panel panel-heading palette-{!! $clubEvent->section->color !!}-700 bg">
+        @elseif ($clubEvent->evnt_type == 1)
+            <div class="panel panel-heading palette-Purple-500 bg">
+        @elseif ($clubEvent->evnt_type == 2
+              || $clubEvent->evnt_type == 3
+              || $clubEvent->evnt_type == 10
+              || $clubEvent->evnt_type == 11)
+            <div class="panel panel-heading palette-{!! $clubEvent->section->color !!}-900 bg">
+        @elseif ($clubEvent->evnt_type == 4
+              || $clubEvent->evnt_type == 5
+              || $clubEvent->evnt_type == 6)
+            <div class="panel panel-heading palette-{!! $clubEvent->section->color !!}-500 bg">
+        @elseif ($clubEvent->evnt_type == 7
+              || $clubEvent->evnt_type == 8)
+            <div class="panel panel-heading palette-{!! $clubEvent->section->color !!}-300 bg">
+        @elseif ($clubEvent->evnt_type == 9)
+            <div class="panel panel-heading palette-{!! $clubEvent->section->color !!}-500 bg">
+        @endif
+            <h4 class="panel-title">
+                @include("partials.event-marker")&nbsp;<a href="{{ URL::route('event.show', $clubEvent->id) }}">{{ $clubEvent->evnt_title }}</a>
+            </h4>
+            <h5 class="panel-title">{{ $clubEvent->evnt_subtitle }}</h5>
+        </div>
+	@endif
+
 	<div class="panel panel-body">
-		<i>{{ trans('mainLang.begin') }}:</i> {{ strftime("%a, %d. %b", strtotime($clubEvent->evnt_date_start)) }}
+		<strong>{{ trans('mainLang.begin') }}:</strong> {{ strftime("%a, %d. %b", strtotime($clubEvent->evnt_date_start)) }}
 		um {{ date("H:i", strtotime($clubEvent->evnt_time_start)) }}
 		<br />
-		<i>{{ trans('mainLang.end') }}:</i> {{ strftime("%a, %d. %b", strtotime($clubEvent->evnt_date_end)) }}
+		<strong>{{ trans('mainLang.end') }}:</strong> {{ strftime("%a, %d. %b", strtotime($clubEvent->evnt_date_end)) }}
 		um {{ date("H:i", strtotime($clubEvent->evnt_time_end)) }}
 		<br />
-		<i>{{ trans('mainLang.club') }}:</i> {{ $clubEvent->section->title }}
+		<strong>{{ trans('mainLang.club') }}:</strong> {{ $clubEvent->section->title }}
 	</div>
 </div>
-
