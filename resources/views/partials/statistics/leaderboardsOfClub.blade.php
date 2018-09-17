bfed943d0ca7548291e07c18fc4257e5b34487d9
4D44H <4d44hex@gmail.com>
2018-09-17 22:36:17 +0200

    Add safer formatting for section titles in statistics

--- resources/views/partials/statistics/leaderboardsOfClub.blade.php
+++ resources/views/partials/statistics/leaderboardsOfClub.blade.php
@@ -1,4 +1,4 @@
-<div class="tab-pane fade in {{ Lara\Section::current()->title === $name? 'active': '' }}" id="{{$name}}Leaderboards">
+<div class="tab-pane fade in {{ Lara\Section::current()->title === $name? 'active': '' }}" id="{{$name}}-leaderboards">
     <table class="table table-hover">
         <thead>
         <tr>
