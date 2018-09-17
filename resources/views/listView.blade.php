47a3d5ff77db7987b71e149708a8b2fa9ed81bb6
4D44H <4d44hex@gmail.com>
2018-09-17 16:13:20 +0200

    Fix font size and alignment in day view title

--- resources/views/listView.blade.php
+++ resources/views/listView.blade.php
@@ -10,11 +10,11 @@
 
 @section('content')
 
-        <h3>
+        <h6 align="center">
                 <a href="/calendar/{{$previous}}" class="btn btn-default hidden-print"> << </a>
                 {{ trans(count($events) == 0 ? 'mainLang.noEventsOn' : 'mainLang.EventsFor') }} {{ $date }}
                 <a href="/calendar/{{$next}}" class="btn btn-default hidden-print"> >> </a>
-        </h3>
+        </h6>
         @foreach($events as $clubEvent)
                 @if ($loop->first)
                         <center>{!! $events->render() !!}</center>
