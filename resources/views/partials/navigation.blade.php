19bb69551efeb5c9a91de3db916dee9d57b8908e
fabian <f.markert87@googlemail.com>
2018-09-24 01:42:21 +0200

    navbar is expanded by default

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-static-top fixed-top">
+<nav class="navbar navbar-expand-sm navbar-light bg-lightfixed-top">
   <div class="container">
     <div class="navbar-header">
         <button type="button"
