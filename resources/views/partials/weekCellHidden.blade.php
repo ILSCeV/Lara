8e23d8ea99640162cac2f663715da4533fff5916
fabian <f.markert87@googlemail.com>
2018-10-10 00:38:20 +0200

    refactor weekcellview

--- resources/views/partials/weekCellHidden.blade.php
+++ resources/views/partials/weekCellHidden.blade.php
@@ -1,11 +1,6 @@
-<div class="bg-light">
+<div class="card bg-light">
 	{{-- Check if the event is still going on and set card color --}}
-	@if( strtotime($clubEvent->evnt_date_end.' '.$clubEvent->evnt_time_end) < time() )
-		<div class=" card-header palette-Grey-500 bg white-text past-event">
-	@else
-		<div class=" card-header palette-Grey-500 bg white-text">
-	@endif
-
+    <div class=" card-header palette-Grey-500 bg white-text @if(strtotime($clubEvent->evnt_date_end.' '.$clubEvent->evnt_time_end) < time()) past-event @endif">
 		<h4 class="card-title">
 			<i class="fa fa-eye-slash">&nbsp;&nbsp;</i><span class="name">{{ trans('mainLang.internalEventP') }}</span>
 		</h4>
