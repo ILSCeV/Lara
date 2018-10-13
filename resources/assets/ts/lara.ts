95b5975531b20fed85f3050fa897cb73b0b5e214
fabian <f.markert87@googlemail.com>
2018-10-13 16:19:27 +0200

    Revert "import bootstrap-select staticly"
    This reverts commit 3a02311

--- resources/assets/ts/lara.ts
+++ resources/assets/ts/lara.ts
@@ -4,7 +4,7 @@ import ("popper.js/dist/umd/popper");
 import ("popper.js/dist/umd/popper-utils");
 import ("tooltip.js/dist/umd/tooltip");
 import ("bootstrap");
-import 'bootstrap-select';
+import ("bootstrap-select");
 import ("bootbox");
 import ("sortablejs");
 import ("cookieconsent");
