05df996a76c82b48fdc337f072c1f603611deac3
4D44H <4d44hex@gmail.com>
2018-10-14 18:04:04 +0200

    Fix footer layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,42 +1,41 @@
-<footer class="navbar-default navbar-static-bottom hidden-print" id="footer">
+<footer class="navbar sticky-bottom navbar-light bg-light hidden-print" id="footer">
     <div class="container text-dark-grey text-center">
-        <br>
-        <span class="col" style="text-align: center;">
+        <span class="col-xs-12 col-sm-12 col-md-3" style="text-align: center;">
             <small>
                 {{ File::exists("gitrevision.txt") ? File::get("gitrevision.txt") : "&nbsp;" }}
             </small>
         </span>
-        <span class="col">
+
+        <span class="col-xs-12 col-sm-12 col-md-3">
             <small>
                 <a href="mailto:lara@il-sc.de">
                     {{ trans('mainLang.notWorkingMail',['Name' => 'Lara']) }}
                 </a>
             </small>
         </span>
-        <span class="col" >
+
+        <span class="col-xs-12 col-sm-12 col-md-3" >
             <small>
                 <a href="https://github.com/ILSCeV/Lara">
                     {{ trans('mainLang.moreInfosProjectsite') }}
                 </a>
             </small>
         </span>
-        <span class="col" >
+
+        <span class="col-xs-12 col-sm-12 col-md-2" >
             <small>
                 <a href=" {{route('lara.privacy')}}">
                     {{ trans('mainLang.privacyPolicy') }}
                 </a>
             </small>
         </span>
-        <span class="col-xs-12 col-sm-12 col-md-1 text-dark-grey" style="text-align: center;">
+
+        <span class="col-xs-12 col-sm-12 col-md-1">
             <small>
                 <a href="{{route('lara.impressum')}}">
                     {{ trans('mainLang.impressum') }}
                 </a>
             </small>
         </span>
-        <br class="d-block d-sm-none d-none d-sm-block d-md-none">
-        <br class="d-block d-sm-none d-none d-sm-block d-md-none">
-        <br>
-        <br>
     </div>
 </footer>
