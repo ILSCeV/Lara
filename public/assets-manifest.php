6d57767201b8a38324f1e4d59a94f1cee4618edf
Stefan Heyder <stefan.heyder@gmail.com>
2018-10-16 21:42:45 +0200

    Remove faulty clubmangement view

---
+++ public/assets-manifest.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+* Built by webpack-php-manifest 
+* Class WebpackBuiltFiles
+*/
+class WebpackBuiltFiles {
+  static $jsFiles = [
+    'survey' => 'survey.js',
+    'lara' => 'lara.js',
+  ];
+  static $cssFiles = [
+    'survey' => 'survey.css',
+    'lara' => 'lara.css',
+  ];
+}
