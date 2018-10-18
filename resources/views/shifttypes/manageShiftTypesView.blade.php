ebb9ff60e334ca5639529c66f1b6d642330b6ab2
Stefan Heyder <stefan.heyder@gmail.com>
2018-10-18 22:11:49 +0200

    Implement bootstrap table for shifttypes management

--- resources/views/shifttypes/manageShiftTypesView.blade.php
+++ resources/views/shifttypes/manageShiftTypesView.blade.php
@@ -15,35 +15,27 @@
 				<h4 class="card-title">{{ trans('mainLang.management') }}: {{ trans('mainLang.shiftTypes') }}</h4>
 		</div>
 		<div class="card card-body p-0">
-            {{Form::open( ['id'=>'shiftTypeFilterForm','class'=>' pt-2 form-inline rounded', 'route'=>'searchShiftType'] ) }}
-            <div class="form-group col-md-2">
-                {{ Form::text('filter','',['class'=>'form-control border', 'placeholder'=>trans('mainLang.search')]) }}
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-success"> {{trans('mainLang.search') }}</button>
-            </div>
-            {{Form::close()}}
             <div class="clearfix pt-2"></div>
-            <div class="table-responsive">
-                <table class="table info table-hover table-sm">
+            <div>
+                <table data-toggle="table" data-search="true">
                     <thead>
                     <tr class="active">
-                        <th class="col-md-1 col-xs-1 text-center">
+                        <th data-sortable="true">
                             #
                         </th>
-                        <th class="col-md-3 col-xs-3 text-center">
+                        <th data-searchable="true" data-field="shift">
                             {{ trans('mainLang.shift') }}
                         </th>
-                        <th class="col-md-2 col-xs-2 text-center">
+                        <th data-searchable="true">
                             {{ trans('mainLang.start') }}-{{ trans('mainLang.end') }}
                         </th>
-                        <th class="col-md-1 col-xs-1 text-center">
+                        <th data-searchable="true">
                             {{ trans('mainLang.weight') }}
                         </th>
-                        <th class="col-md-2 col-xs-2 text-center">
+                        <th>
                             {{ trans("mainLang.actions") }}
                         </th>
-                        <th class="col-md-2 col-xs-2 text-center">
+                        <th data-searchable="false">
                             {{ trans("mainLang.replaceAll") }}
                         </th>
                     </tr>
@@ -53,23 +45,23 @@
                     <div class="container">
                         @foreach($shiftTypes as $shiftType)
                             <tr>
-                                <td class="text-center">
+                                <td >
                                     {!! $shiftType->id !!}
                                 </td>
-                                <td class="text-center">
+                                <td >
                                     <a href="../shiftType/{{ $shiftType->id }}">
                                         {!! $shiftType->title !!}
                                     </a>
                                 </td>
-                                <td class="text-center">
+                                <td >
                                     {!! date("H:i", strtotime($shiftType->start)) !!}
                                     -
                                     {!! date("H:i", strtotime($shiftType->end)) !!}
                                 </td>
-                                <td class="text-center">
+                                <td >
                                     {!! $shiftType->statistical_weight !!}
                                 </td>
-                                <td class="text-center">
+                                <td >
                                     <a href="../shiftType/{{ $shiftType->id }}"
                                        class="btn btn-small btn-success"
                                        rel="nofollow">
@@ -85,7 +77,7 @@
                                         {{ trans('mainLang.deleteThisShiftType') }}
                                     </a>
                                 </td>
-                                <td class="text-center">
+                                <td >
                                     @include('shifttypes.shiftTypeSelect',['shift'=>$shiftType,'shiftTypes' => $shiftTypes,'route'=>'completeOverrideShiftType','shiftTypeId'=>$shiftType->id, 'selectorClass'=>'shiftTypeReplaceSelector'])
                                 </td>
                             </tr>
@@ -95,10 +87,6 @@
                 </table>
             </div>
 		</div>
-	</div>
-
-	<div class="text-center">
-		{{ $shiftTypes->links() }}
 	</div>
 
 	<br/>
