7abfd8dc05611a96781adee23f7bfac9e35c7e91
fabian <f.markert87@googlemail.com>
2019-01-05 23:29:35 +0100

    center progressbar

--- resources/views/monthView.blade.php
+++ resources/views/monthView.blade.php
@@ -36,7 +36,7 @@
     </div>
 
     <div id="monthTableContainer" class="container-fluid">
-        <div class="progress col-xs-12 col-md-5">
+        <div class="progress col-xs-12 col-md-5 mx-auto" style="width: 100%">
             <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
         </div>
     </div>
