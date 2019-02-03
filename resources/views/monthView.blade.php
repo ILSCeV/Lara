615faf38128672c9883bb801c9692138f4a9678c
fabian <f.markert87@googlemail.com>
2019-02-03 19:00:55 +0100

    replace load bar with spinner

--- resources/views/monthView.blade.php
+++ resources/views/monthView.blade.php
@@ -36,8 +36,8 @@
     </div>
 
     <div id="monthTableContainer" class="container-fluid">
-        <div class="progress col-xs-12 col-md-5 mx-auto" style="width: 100%">
-            <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+        <div class="text-center">
+            <div class="fa fa-spinner fa-spin"></div>
         </div>
     </div>
 
