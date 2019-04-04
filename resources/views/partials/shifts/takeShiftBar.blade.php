72ba175ced9a861585afbec7dc03ab27d0620818
bedo2991 <fra.bdn@gmail.com>
2019-04-04 23:09:05 +0200

    div Instead Of Tables

--- resources/views/partials/shifts/takeShiftBar.blade.php
+++ resources/views/partials/shifts/takeShiftBar.blade.php
@@ -28,110 +28,94 @@ if($hideComments){
     <input type="text" id="{!! 'website' . $shift->id !!}" name="{!! 'website' . $shift->id !!}" value=""/>
 </div>
 
-<table class="table-no-bordered container-fluid">
-    <thead>
-    <tr>
-        <th style="width: 15%"></th>
-        <th style="width: 15%"></th>
-        <th style="width: 25%"></th>
-        <th style="width: 14%"></th>
-        <th style="width: 1%"></th>
-        <th class="{{$commentClass}}" style="width: 45%"></th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>
-            @include("partials.shiftTitle")
-        </td>
-        <td>
-            <div id="clubStatus{{ $shift->id }}">
-                @include("partials.shiftStatus")
-            </div>
-        </td>
-        @if( isset($shift->getPerson->prsn_ldap_id) && !Auth::user())
-            <td>
-                <div class="form-group form-group-sm">
-                    @if($shift->getPerson->isNamePrivate() == 0)
-                        {{-- Shift USERNAME--}}
-                        <div id="{!! 'userName' . $shift->id !!}" class="form-control form-control-sm">
-                            {!! $shift->getPerson->prsn_name !!}
-                        </div>
-                    @else
-                        <div id="{!! 'userName' . $shift->id !!}" class="form-control form-control-sm">
-                            @if(isset($shift->person->user))
-                                {{ trans($shift->person->user->section->title . '.' . $shift->person->user->status) }}
-                            @endif
-                        </div>
+<div class="col-md-3 px-1">
+    @include("partials.shiftTitle")
+</div>
+<div class="col-md-1 p-0">
+    <div id="clubStatus{{ $shift->id }}">
+        @include("partials.shiftStatus")
+    </div>
+</div>
+@if( isset($shift->getPerson->prsn_ldap_id) && !Auth::user())
+    <div class="col-md-4 p-0">
+        <div class="form-group form-group-sm">
+            @if($shift->getPerson->isNamePrivate() == 0)
+                {{-- Shift USERNAME--}}
+                <div id="{!! 'userName' . $shift->id !!}" class="form-control form-control-sm">
+                    {!! $shift->getPerson->prsn_name !!}
+                </div>
+            @else
+                <div id="{!! 'userName' . $shift->id !!}" class="form-control form-control-sm">
+                    @if(isset($shift->person->user))
+                        {{ trans($shift->person->user->section->title . '.' . $shift->person->user->status) }}
                     @endif
                 </div>
-            </td>
-            {{-- SHIFT CLUB --}}
-            <td>
-                <div id="{!! 'club' . $shift->id !!}" class="form-group form-group-sm ">
-                    <div class="form-control form-control-sm">
-                        {!! "(" . $shift->getPerson->getClub->clb_title . ")" !!}
-                    </div>
-                </div>
-            </td>
-            <td>
+            @endif
+        </div>
+    </div>
+    {{-- SHIFT CLUB --}}
+    <div class="col-md-2 p-0">
+        <div id="{!! 'club' . $shift->id !!}" class="form-group form-group-sm ">
+            <div class="form-control form-control-sm">
+                {!! "(" . $shift->getPerson->getClub->clb_title . ")" !!}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-1 p-0">
             <span class="float-left">
 		   @if($shift->comment === "") <i class="fas fa-comment-alt"></i> @else <i class="fas fa-comment"></i> @endif
             &nbsp;&nbsp;
 		</span>
-            </td>
-            {{-- COMMENT SECTION --}}
-            <td>
-                <div class="form-group from-group-sm hidden-print word-break ">
-                    <div class="form-control form-control-sm">
+    </div>
+    {{-- COMMENT SECTION --}}
+    <div class="col-md-10 col-md-offset-1">
+        <div class="form-group from-group-sm hidden-print word-break ">
+            <div class="form-control form-control-sm">
                         <span class="w-auto @if(isset($hideComments)) hide @endif" id="{{'comment'.$shift->id}}"
-                          name="{{'comment' . $shift->id}}">
+                              name="{{'comment' . $shift->id}}">
 		                {!! !empty($shift->comment) ? $shift->comment : "-" !!}
 		                </span>
-                    </div>
-                </div>
-            </td>
-        @else
-            {{-- show everything for members --}}
-            {{-- SHIFT STATUS, USERNAME, DROPDOWN USERNAME and LDAP ID --}}
-            <td>
-                <div class="form-group form-group-sm">
-                    @include('partials.shifts.shiftName',['shift'=>$shift])
-                </div>
-            </td>
-            {{-- SHIFT CLUB and DROPDOWN CLUB --}}
-            <td>
-                <div class="form-group form-group-sm ">
-                    @include('partials.shifts.shiftClub',['shift'=>$shift])
-                </div>
-            </td>
-            {{-- COMMENT SECTION --}}
-            {{-- Hidden comment field to be opened after the click on the icon
-                 see vedst-scripts "Show/hide comments" function --}}
-            <td>
-                <div class="form-control form-control-sm">
+            </div>
+        </div>
+    </div>
+@else
+    {{-- show everything for members --}}
+    {{-- SHIFT STATUS, USERNAME, DROPDOWN USERNAME and LDAP ID --}}
+    <div class="col-md-4 px-1">
+        <div class="form-group form-group-sm">
+            @include('partials.shifts.shiftName',['shift'=>$shift])
+        </div>
+    </div>
+    {{-- SHIFT CLUB and DROPDOWN CLUB --}}
+    <div class="col-md-2 p-0">
+        <div class="form-group form-group-sm ">
+            @include('partials.shifts.shiftClub',['shift'=>$shift])
+        </div>
+    </div>
+    {{-- COMMENT SECTION --}}
+    {{-- Hidden comment field to be opened after the click on the icon
+         see vedst-scripts "Show/hide comments" function --}}
+    <div class="col-md-1 p-0">
+        <div class="form-control form-control-sm">
                 <span class="float-left">
                  @if($shift->comment === "") <i class="fas fa-comment-alt"></i> @else <i
-                        class="fas fa-comment"></i> @endif
+                            class="fas fa-comment"></i> @endif
                     &nbsp;&nbsp;
                 </span>
-                </div>
-            </td>
-            <td>
-                <div class="form-group from-group-sm hidden-print word-break ">
+        </div>
+    </div>
+    <div class="col-md-10 col-md-offset-1">
+        <div class="form-group from-group-sm hidden-print word-break ">
 
-                    {!! Form::text('comment' . $shift->id,
-                                               $shift->comment,
-                                               ['placeholder'=>Lang::get('mainLang.addCommentHere'),
-                                                     'id'=>'comment' . $shift->id,
-                                                      'name'=>'comment' . $shift->id,
-                                                     'class'=>'form-control form-control-sm '. $commentClass])
-                                    !!}
+            {!! Form::text('comment' . $shift->id,
+                                       $shift->comment,
+                                       ['placeholder'=>Lang::get('mainLang.addCommentHere'),
+                                             'id'=>'comment' . $shift->id,
+                                              'name'=>'comment' . $shift->id,
+                                             'class'=>'form-control form-control-sm '. $commentClass])
+                            !!}
 
-                </div>
-            </td>
-        @endif
-    </tr>
-    </tbody>
-</table>
+        </div>
+    </div>
+@endif
 {!! Form::close() !!}
