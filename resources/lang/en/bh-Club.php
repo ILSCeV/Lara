b628ab2bcdba5a461bbf0b2d62d41dccfad64eb9
fabian <f.markert87@googlemail.com>
2019-05-03 23:38:35 +0200

    add bh translation

---
+++ resources/lang/en/bh-Club.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'candidate' => 'Candidate',
+    'member' => 'Active',
+    'veteran' => 'Pensioner',
+    'resigned' => 'Resigned',
+    'ex-member' => 'ex-Member',
+    'ex-candidate' => 'ex-Candidate',
+    'external' => 'External',
+    'guest' => 'Guest'
+];
