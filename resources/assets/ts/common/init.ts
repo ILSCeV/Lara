878e3efe07e4c1a9a86c7b0a7e85a99d0cb3c266
fabian <f.markert87@googlemail.com>
2019-06-27 01:18:18 +0200

    fix filter bug, can be hidden by clicking on button

--- resources/assets/ts/common/init.ts
+++ resources/assets/ts/common/init.ts
@@ -3,6 +3,15 @@ $(window).on({
     $('#userGroupDevelop').selectpicker({
       style: 'btn-sm',
     });
+
+    // Show/hide comments
+    $('.showhide').on({
+      click: function (e) {
+        let comment = $(e.target).closest('.shiftRow').find('[name^=comment]');
+        comment.toggleClass("hide", comment.is(":visible"));
+        // isotope.layout();
+      }
+    });
   }
 });
 
