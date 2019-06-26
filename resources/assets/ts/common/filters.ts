39631fa3b8b5295140116b2a9d56f6204319da48
fabian <f.markert87@googlemail.com>
2019-06-27 01:18:18 +0200

    fix filter bug, can be hidden by clicking on button

--- resources/assets/ts/common/filters.ts
+++ resources/assets/ts/common/filters.ts
@@ -192,15 +192,6 @@ export const initFilters = () => {
         weekStart.setToggleStatus(safeGetLocalStorage("weekStart") == "monday");
     }
 
-    // Show/hide comments
-    $('.showhide').on({
-      click: function (e) {
-        let comment = $(e.target).closest('.shiftRow').find('[name^=comment]');
-        comment.toggleClass("hide", comment.is(":visible"));
-        // isotope.layout();
-      }
-    });
-
     // button to remove events from week view - mostly for printing
     $('.hide-event').on({
       click: function (e) {
