63a06333581f41afc690c9f540d15028d0af057b
Fabian Markert <f.markert87@googlemail.com>
2020-10-25 17:02:03 +0100

    Feature/new week rythm (#310)
    * change week rythm to new coffee rythm
    
    new week rythm is from tuesday to monday
    
    * change week rythm to new coffee rythm
    
    new week rythm is from tuesday to monday

--- resources/views/partials/weekView/survey.blade.php
+++ resources/views/partials/weekView/survey.blade.php
@@ -3,12 +3,12 @@
         $elementClass = 'element-item section-filter section-survey flex-grow-1 ';
 
     if ( date('W', strtotime($survey->deadline)) === $date['week']
-     &&  date('N', strtotime($survey->deadline)) < 3 ) {
+     &&  date('N', strtotime($survey->deadline)) < 2 ) {
         $elementClass .= "week-mo-so ";
         }
     elseif ( date("W", strtotime($survey->deadline) ) === date("W", strtotime("next Week".$weekStart))
-     &&      date('N', strtotime($survey->deadline)) < 3 ){
-        $elementClass.= "week-mi-di hide";
+     &&      date('N', strtotime($survey->deadline)) < 2 ){
+        $elementClass.= "week-di-mo hide";
         }
     @endphp
     <div class="p-2 mb-3 {{ $elementClass }}">
