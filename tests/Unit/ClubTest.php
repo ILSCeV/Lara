a523268caa9e160e80c37b50907a3779ac715bb0
fabian <f.markert87@googlemail.com>
2023-03-12 19:23:34 +0100

    💚  php unit test erstellt

---
+++ tests/Unit/ClubTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Tests\CreatesApplication;
+
+class ClubTest extends TestCase
+{
+    use CreatesApplication;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
