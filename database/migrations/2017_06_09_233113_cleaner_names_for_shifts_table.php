faef5e95806e4cab69fa6c41b0258874cca1b5ea
fabian <f.markert87@googlemail.com>
2023-03-12 04:24:49 +0100

    fix migration

--- database/migrations/2017_06_09_233113_cleaner_names_for_shifts_table.php
+++ database/migrations/2017_06_09_233113_cleaner_names_for_shifts_table.php
@@ -13,13 +13,13 @@ class CleanerNamesForShiftsTable extends Migration
      */
     public function up()
     {
-        alterShiftTable("schdl_id", "schedule_id");
-        alterShiftTable("jbtyp_id", "shifttype_id");
-        alterShiftTable("prsn_id", "person_id");
-        alterShiftTable("entry_user_comment", "comment");
-        alterShiftTable("entry_statistical_weight", "statistical_weight");
-        alterShiftTable("entry_time_start", "start");
-        alterShiftTable("entry_time_end", "end");
+        $this->alterShiftTable("schdl_id", "schedule_id");
+        $this->alterShiftTable("jbtyp_id", "shifttype_id");
+        $this->alterShiftTable("prsn_id", "person_id");
+        $this->alterShiftTable("entry_user_comment", "comment");
+        $this->alterShiftTable("entry_statistical_weight", "statistical_weight");
+        $this->alterShiftTable("entry_time_start", "start");
+        $this->alterShiftTable("entry_time_end", "end");
     }
 
     /**
@@ -30,13 +30,13 @@ class CleanerNamesForShiftsTable extends Migration
     public function down()
     {
 
-        alterShiftTable("schedule_id", "schdl_id");
-        alterShiftTable("shifttype_id", "jbtyp_id");
-        alterShiftTable("person_id", "prsn_id");
-        alterShiftTable("comment", "entry_user_comment");
-        alterShiftTable("statistical_weight", "entry_statistical_weight");
-        alterShiftTable("start", "entry_time_start");
-        alterShiftTable("end", "entry_time_end");
+        $this->alterShiftTable("schedule_id", "schdl_id");
+        $this->alterShiftTable("shifttype_id", "jbtyp_id");
+        $this->alterShiftTable("person_id", "prsn_id");
+        $this->alterShiftTable("comment", "entry_user_comment");
+        $this->alterShiftTable("statistical_weight", "entry_statistical_weight");
+        $this->alterShiftTable("start", "entry_time_start");
+        $this->alterShiftTable("end", "entry_time_end");
 
     }
 
