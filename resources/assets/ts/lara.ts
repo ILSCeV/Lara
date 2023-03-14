52740f3e0835ae839fe55ece92b8da56c07efae3
fabian <f.markert87@googlemail.com>
2023-03-14 03:00:44 +0100

    🐛 bootbox tables have to be removed from imports

--- resources/assets/ts/lara.ts
+++ resources/assets/ts/lara.ts
@@ -5,7 +5,7 @@ import "popper.js/dist/umd/popper-utils"
 import "tooltip.js/dist/umd/tooltip"
 import "bootstrap";
 import "bootstrap-select";
-import "bootstrap-table"
+
 import "bootbox"
 import "sortablejs"
 import "cookieconsent"
