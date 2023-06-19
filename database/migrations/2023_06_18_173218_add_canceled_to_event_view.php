05a2ad9df138e591b116c10e0e3137e719cd371c
fabian <f.markert87@googlemail.com>
2023-06-19 02:31:58 +0200

    view korrigiert
    nur noch bestimmte typen zugelassen

--- database/migrations/2023_06_18_173218_add_canceled_to_event_view.php
+++ database/migrations/2023_06_18_173218_add_canceled_to_event_view.php
@@ -18,7 +18,7 @@ class AddCanceledToEventView extends Migration
             create or replace VIEW EVENT_VIEW AS
             SELECT
                 CONCAT(
-                SUBSTRING(MD5(ce.id), 1, 8),
+                            SUBSTRING(MD5(ce.id), 1, 8),
                 '-',
                 SUBSTRING(MD5(ce.id), 9, 4),
                 '-4',
@@ -27,20 +27,20 @@ class AddCanceledToEventView extends Migration
                 SUBSTRING(MD5(ce.id), 17, 3),
                 '-',
                 SUBSTRING(MD5(ce.id), 21, 12)
-            ) AS import_id,
+                        ) AS import_id,
                 ce.evnt_title name,
                 ce.evnt_date_start start,
                 ce.evnt_time_start start_time,
                 ce.evnt_date_end
             end,
-            ce.evnt_time_end end_time,
-            club.clb_title place,
-            ce.evnt_public_info marquee ,
-            ce.event_url link,
-            ce.canceled cancelled,
-            ce.updated_at updated_on,
-            s.id section_id,
-            CASE
+                        ce.evnt_time_end end_time,
+                        club.clb_title place,
+                        ce.evnt_public_info marquee ,
+                        ce.event_url link,
+                        ce.canceled cancelled,
+                        ce.updated_at updated_on,
+                        s.id section_id,
+                        CASE
                 when ce.evnt_type = 0 then 'calendar-days'
                 when ce.evnt_type = 1 then 'info'
                 when ce.evnt_type = 2 then 'star'
@@ -55,35 +55,36 @@ class AddCanceledToEventView extends Migration
                 when ce.evnt_type = 11 then 'utensils'
                 ELSE null
             END
-            icon
+                        icon
             FROM
-            club_events ce
-            join clubs club on
-            club.id = ce.plc_id
+                        club_events ce
             join sections s on
-            s.id = ce.plc_id
+                        s.id = ce.plc_id
+            join clubs club on
+                        club.clb_title = s.title
             where
-             ce.evnt_is_private = 0
+                        ce.evnt_is_private = 0
+            and ce.evnt_type not in (1, 4, 5, 6, 7, 9)
             AND (
-            STR_TO_DATE(CONCAT(ce.evnt_date_start,
-            ' ',
-            ce.evnt_time_start),
-            '%Y-%m-%d %H:%i:%s') > now()
+                        STR_TO_DATE(CONCAT(ce.evnt_date_start,
+                        ' ',
+                        ce.evnt_time_start),
+                        '%Y-%m-%d %H:%i:%s') > now()
                 or
-            STR_TO_DATE(CONCAT(ce.evnt_date_end,
+                        STR_TO_DATE(CONCAT(ce.evnt_date_end,
                 ' ',
                 ce.evnt_time_end),
                 '%Y-%m-%d %H:%i:%s') > now())
             order by
-            STR_TO_DATE(CONCAT(ce.evnt_date_start,
-            ' ',
-            ce.evnt_time_start),
-            '%Y-%m-%d %H:%i:%s'),
-            STR_TO_DATE(CONCAT(ce.evnt_date_end,
-            ' ',
-            ce.evnt_time_end),
-            '%Y-%m-%d %H:%i:%s')
-            ;
+                        STR_TO_DATE(CONCAT(ce.evnt_date_start,
+                        ' ',
+                        ce.evnt_time_start),
+                        '%Y-%m-%d %H:%i:%s'),
+                        STR_TO_DATE(CONCAT(ce.evnt_date_end,
+                        ' ',
+                        ce.evnt_time_end),
+                        '%Y-%m-%d %H:%i:%s')
+                        ;
     SQL;
 
         DB::statement($sql);
